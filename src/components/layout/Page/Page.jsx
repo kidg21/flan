47c3c3dc294c7ab029d8b992e0962a320709cc9d
Greg Person <gperson@digmap.com>
2019-03-14 21:40:15 -0700

    Page - Added component, 'Page'. - Added 'Page' stories to Storybook.

---
+++ src/components/layout/Page/Page.jsx
@@ -0,0 +1,54 @@
+import styled, { css } from "styled-components";
+import { fonts, colors } from "../../base/Variables/Variables";
+import { PlaceholderText } from "../../_helpers/Placeholders";
+
+const Page = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  background: ${colors.white};
+  padding: 1rem;
+  > h1,
+  > h2,
+  > h3,
+  > h4,
+  > h5,
+  > h6,
+  > p {
+    margin: 0;
+  }
+  /* Prototype Content - displays when a Card is empty */
+  &:empty {
+    &:before {
+      white-space: pre;
+      ${PlaceholderText}
+      content: "{ Page } \00000A 'The CardList's less 'Card-y' cousin";
+    }
+    &:after {
+      ${PlaceholderText}
+      padding: 0;
+      text-align: initial;
+      white-space: pre-line;
+      content: "Just think about these things in your mind - then bring them into your world. Isn't that fantastic? You can just push a little tree out of your brush like that. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. I thought today we would make a happy little stream that's just running through the woods here. Just a little indication.
+      \00000A
+      \00000A
+      This is probably the greatest thing to happen in my life - to be able to share this with you.  Here's something that's fun. Talk to trees, look at the birds. Whatever it takes. It's so important to do something every day that will make you happy.
+      \00000A
+      \00000A
+      All you have to learn here is how to have fun. Now, we're going to fluff this cloud. Put it in, leave it alone. Trees get lonely too, so we'll give him a little friend.
+      \00000A
+      \00000A
+      Let's get crazy. When things happen - enjoy them. They're little gifts. Steve wants reflections, so let's give him reflections. All you need to paint is a few tools, a little instruction, and a vision in your mind. Everybody needs a friend. This is your world.
+      \00000A
+      \00000A
+      Every day I learn. Trees live in your fan brush, but you have to scare them out. You can do it. A fan brush is a fantastic piece of equipment. Use it. Make friends with it.
+      \00000A
+      \00000A
+      Little trees and bushes grow however makes them happy. Just let your mind wander and enjoy. This should make you happy. You don't have to spend all your time thinking about what you're doing, you just let it happen. Let's make a happy little mountain now. If I paint something, I don't want to have to explain what it is.
+      \00000A
+      \00000A
+      It all happens automatically. You can create beautiful things - but you have to see them in your mind first. There we are. Nature is so fantastic, enjoy it. Let it make you happy. Let's put some highlights on these little trees. The sun wouldn't forget them. With practice comes confidence.";
+    }
+  }
+`;
+
+export default Page;
