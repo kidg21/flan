4f1e797e2dfccc3eab1e9ca89f30c45994f80c36
Greg Person <gperson@digmap.com>
2019-03-19 10:49:45 -0700

    Form - Created a layout component and 'form parts'. - Added story.

--- src/components/layout/Form/Form.jsx
+++ src/components/layout/Form/Form.jsx
@@ -1,52 +1,91 @@
-import React from 'react'
-import styled from 'styled-components'
-//import Button from './Button/NewButton'
-
+import React from "react";
+import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
+import { fonts, colors, shadows } from "../../base/Variables/Variables";
+import { PlaceholderText } from "../../_helpers/Placeholders";
 
 const Form = styled.form`
-  margin: 0 auto;
-  width: 50%;
-  min-width: 400px;
-  max-width: 800px;
-  text-align: center;
-  border: 1px solid #ddd;
-  padding-top: 0px;
-  padding-bottom: 10px;
-  color: black;
-  background: white;
-`;
-
-const Title = styled.h2`
-  margin-top: 20px;
-  margin-bottom: 20px;
-  font-size: 1.5em;
-  color: black;
-  background-color: white;
-`;
-
-
-const Input = styled.input`
-  font-size: 1.45em;
-  border: 1px solid #ddd;
-`;
-
-
-  export default class SignupForm extends React.Component {
-
-    signUp = (e) => {
-      const email = new FormData(e.target).get('email');
-      e.preventDefault();
-      console.log(`New signup from ${email}`);
-    }
-  
-    render() {
-      return (
-        <Form onSubmit={this.signUp}>
-          <Title>
-            Sign up to my newsletter
-          </Title>
-          <Input type="email" name="email" />
-        </Form>
-      );
+  display: grid;
+  grid-gap: .5rem;
+  flex: auto;
+  align-content: flex-start;
+  padding: 1rem 1.5rem;
+  background-color: ${props => (props.white ? colors.white : "")};
+  ${props =>
+    props.dark &&
+    css`
+      color: ${colors.grey_light};
+      background-color: ${props => colors.grey_dark};
+      * {
+        color: ${colors.grey_light} !important;
+      }
+    `}
+  /* Prototype Content - displays when a Form is empty */
+  &:empty {
+    &:before {
+      ${PlaceholderText}
+      content: "{ Form } \00000A 'Displays a grid of user inputs in responsive columns'";
     }
   }
+`;
+
+const Title = styled.h3`
+  color: ${colors.grey_60};
+  letter-spacing: 2px;
+  margin: 0;
+`;
+const FormTitle = props => <Title>{props.title}</Title>;
+
+const InputBlock = styled.section`
+  display: grid;
+  grid-gap: .75rem;
+  margin-bottom: .5rem;
+  /* Prototype Content - displays when a Form is empty */
+  &:empty {
+    &:before {
+      ${PlaceholderText}
+      content: "{ InputBlock } \00000A ''";
+    }
+  }
+`;
+
+const Label = styled.h5`
+  color: ${colors.grey_40};
+  font-weight: 800;
+  letter-spacing: 2px;
+  margin-bottom: 0;
+`;
+const InputBlockLabel = props => <Label>{props.label}</Label>;
+
+const InputGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+  grid-template-columns: ${props => (props.fullWidth ? "repeat(1, 1fr)" : "")};
+  grid-gap: 1rem;
+  /* Prototype Content - displays when a Form is empty */
+  &:empty {
+    &:before {
+      ${InputGroup}
+      content: "{ InputGroup } \00000A ''";
+    }
+  }
+`;
+
+FormTitle.defaultProps = {
+  title: "Form Title"
+};
+InputBlockLabel.defaultProps = {
+  label: "Input Block Label"
+};
+
+FormTitle.propTypes = {
+  title: PropTypes.string
+};
+InputBlockLabel.propTypes = {
+  label: PropTypes.string.isRequired
+};
+InputGroup.propTypes = {
+  fullWidth: PropTypes.string
+};
+
+export { Form as default, FormTitle, InputBlock, InputBlockLabel, InputGroup };
