65fa7ec73a28b51ceda1f6c11f4e299fcc47a63e
Greg Person <gperson@digmap.com>
2019-03-19 10:35:01 -0700

    Updates from Liz

---
+++ src/components/blocks/Accordion/IconGroup.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import PropTypes from 'prop-types';
+import Icon from '../../atoms/Icon/Icon';
+
+
+
+const defaultProps = {
+    commandOneProps: {nameProp: ["far", "expand-arrows"],
+    colorProp: "darkgray"},
+commandTwoProps: {nameProp: ["far", "share"],
+    colorProp: "darkgray"},
+commandThreeProps: {nameProp: ["far", "filter"],
+    colorProp: "darkgray"}
+};
+
+const Group = styled.div`
+display: flex; 
+width: 5em;
+padding-left: 5px;
+padding-right: 5px;
+justify-content: space-between;
+`
+
+
+function IconGroup(props) {
+    return (
+        <Group>
+          <Icon  {...props.commandOneProps}/>
+          <Icon  {...props.commandTwoProps}/>
+          <Icon  {...props.commandThreeProps}/>
+        </Group>
+    )
+  }
+
+IconGroup.defaultProps = defaultProps;
+export default IconGroup;
