68a57230ba3f23e3b0a9c0341e334822075c5d59
Greg Person <gperson@digmap.com>
2019-04-01 17:31:04 -0700

    Select Menu ( new ) - Configured the styling of the 'react-select' component to match the rest of the input elements. - Configured props - Added Stories - Configured Knobs - Removed old files

--- src/components/atoms/Dropdown/Dropdown.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import DropdownClassic from "./Dropdown";
-import InlineDropdown from "./InlineDropdown";
-
-// Colors
-storiesOf("Atoms|Dropdown", module)
-.add("Classic Dropdown", () => <DropdownClassic/>)
-.add("Inline Dropdown", () => <InlineDropdown/>);
