62765633b61e26fc4fb5c051b9800c8a4a533e81
Greg Person <gperson@digmap.com>
2019-04-01 17:33:31 -0700

    Input - Added options for pre and post-fix Select Menus

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import styled, { css } from "styled-components";
-import PropTypes from "prop-types";
-import { HelpText, ErrorText } from "../../layout/Form/Form";
-import { colors, shadows } from "../../../attributes/Variables/Variables";
+import React from "react"
+import styled, { css } from "styled-components"
+import PropTypes from "prop-types"
+import { HelpText, ErrorText } from "../../layout/Form/Form"
+import SelectMenu from "../SelectMenu"
+import { colors, shadows } from "../../../attributes/Variables/Variables"
 
 const TextInputContainer = styled.div`
   display: grid;
@@ -15,12 +16,16 @@ const TextInputContainer = styled.div`
       ? "minmax(auto, auto) minmax(auto, 3fr)"
       : props.postfix
       ? "minmax(auto, 3fr) minmax(auto, auto)"
+      : props.preSelect
+      ? "minmax(auto, 2fr) minmax(auto, 3fr)"
+      : props.postSelect
+      ? "minmax(auto, 3fr) minmax(auto, 2fr)"
       : "repeat(1, 1fr)"};
   grid-gap: 0.35rem;
   align-content: flex-start;
   color: ${props => (props.disabled ? colors.grey_40 : "")};
-  margin-bottom: 0.5rem;
-`;
+  /* margin-bottom: 0.5rem; */
+`
 
 const TextInputLabel = styled.label`
   display: flex;
@@ -41,14 +46,12 @@ const TextInputLabel = styled.label`
     font-size: 1.5rem;
     color: ${colors.alert};
   }
-`;
+`
 
 const PrePostLabel = styled.label`
   display: flex;
   justify-content: center;
   align-items: center;
-  /* font-size: small; */
-  /* font-size: 1.1rem; */
   font-weight: bold;
   letter-spacing: 2px;
   text-transform: lowercase;
@@ -56,9 +59,9 @@ const PrePostLabel = styled.label`
   background-color: ${colors.grey_light};
   border: 1px solid ${colors.grey_20};
   border-bottom: 1px solid ${colors.grey_40};
-  padding: 0.25rem 0.5rem;
+  padding: 0.25rem 0.75rem;
   white-space: nowrap;
-`;
+`
 
 const TextInput = styled.input.attrs({ type: "text" })`
   border: 1px solid ${colors.grey_20};
@@ -66,9 +69,14 @@ const TextInput = styled.input.attrs({ type: "text" })`
   border-color: ${props => (props.error ? colors.alert : "")};
   background-color: ${props => (props.error ? "#f9ebeb" : "")};
   caret-color: ${props => (props.error ? colors.alert : "")};
+  min-height: 2.75rem;
   padding: 0.5rem 0.75rem;
   ::placeholder {
     color: ${props => (props.error ? colors.alert : "")};
+  }
+  &:hover {
+    border: 1px solid ${colors.success};
+    border-color: ${props => (props.error ? colors.alert : "")};
   }
   &:focus {
     background-color: ${props => (props.error ? "#f9ebeb" : "#f1f8eb")};
@@ -81,7 +89,7 @@ const TextInput = styled.input.attrs({ type: "text" })`
         props.error ? colors.alert : colors.success};
     }
   }
-`;
+`
 
 function Input({ helpText, errorText, ...props }) {
   return (
@@ -93,7 +101,9 @@ function Input({ helpText, errorText, ...props }) {
       helpText={helpText}
       required={props.required}
       prefix={props.prefix}
+      preSelect={props.preSelect}
       postfix={props.postfix}
+      postSelect={props.postSelect}
       error={props.error}
       twoInputs={props.twoInputs} // 2 inputs in a row
       threeInputs={props.threeInputs} // 3 inputs in a row
@@ -101,6 +111,17 @@ function Input({ helpText, errorText, ...props }) {
       <TextInputLabel {...props}>{props.label}</TextInputLabel>
       {/* Prefix Label (conditional) */}
       {props.prefix ? <PrePostLabel>{props.prefix}</PrePostLabel> : null}
+      {/* Prefix Select Menu (conditional) */}
+      {props.preSelect ? (
+        <SelectMenu
+          displayBlock={true} // Grid Override
+          label={null}
+          name="Choose"
+          isClearable={false}
+          options={props.preOptions}
+          defaultValue={props.preOptions[0]}
+        />
+      ) : null}
       <TextInput {...props} />
       {/* Column 2 (conditional) */}
       {props.twoInputs || props.threeInputs ? (
@@ -112,12 +133,23 @@ function Input({ helpText, errorText, ...props }) {
       ) : null}
       {/* Postfix (conditional) */}
       {props.postfix ? <PrePostLabel>{props.postfix}</PrePostLabel> : null}
+      {/* Postfix Select Menu (conditional) */}
+      {props.postSelect ? (
+        <SelectMenu
+          displayBlock={true} // Grid Override
+          label={null}
+          name="Choose"
+          isClearable={false}
+          options={props.postOptions}
+          defaultValue={props.postOptions[0]}
+        />
+      ) : null}
       {/* Help Text */}
       {helpText ? <HelpText helpText={helpText} /> : null}
       {/* Error Message (required) */}
       {props.error ? <ErrorText errorText={errorText} /> : null}
     </TextInputContainer>
-  );
+  )
 }
 
 Input.defaultProps = {
@@ -131,8 +163,20 @@ Input.defaultProps = {
   error: false,
   errorText: "Error text for the Input component",
   twotwoInputs: false,
-  threeInputs: false
-};
+  threeInputs: false,
+  preSelect: false,
+  preOptions: [
+    { value: "sir", label: "Sir" },
+    { value: "madam", label: "Madam" },
+    { value: "my lord", label: "My Lord" }
+  ],
+  postSelect: false,
+  postOptions: [
+    { value: "com", label: ".com" },
+    { value: "org", label: ".org" },
+    { value: "gov", label: ".gov" }
+  ]
+}
 
 Input.propTypes = {
   label: PropTypes.string.isRequired,
@@ -144,7 +188,11 @@ Input.propTypes = {
   disabled: PropTypes.bool,
   error: PropTypes.bool,
   twoInputs: PropTypes.bool,
-  threeInputs: PropTypes.bool
-};
+  threeInputs: PropTypes.bool,
+  preSelect: PropTypes.string,
+  preOptions: PropTypes.array,
+  postSelect: PropTypes.string,
+  postOptions: PropTypes.array
+}
 
-export { Input as default };
+export { Input as default }
