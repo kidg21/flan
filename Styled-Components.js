3c4047f225acc54d50b737542199c0c68da3aa11
DIGMAP\ESugimoto <ESugimoto@digmap.com>
2019-04-10 12:52:12 -0700

    update catalog

--- Styled-Components.js
+++ Styled-Components.js
@@ -1,2 +1,3 @@
-require("ss-styled-components/widgets/checkbox.jsx");
+require("ss-styled-components/src/components/index");
+// require("ss-styled-components/widgets/checkbox.jsx");
 // require("ss-styled-components/layout/card.jsx");
