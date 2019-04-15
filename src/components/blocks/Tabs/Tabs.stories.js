1dba3b66ecd4f7ee29837ff3c8d69d54927b945b
Greg Person <gperson@digmap.com>
2019-04-15 16:13:34 -0700

    Tabs (new) - Preliminary component design

---
+++ src/components/blocks/Tabs/Tabs.stories.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import { Padding } from "helpers/Display"
+import Tabs, { Tab } from "blocks/Tabs"
+import Button from "atoms/Button"
+
+// Buttons
+storiesOf("Blocks|Tabs", module)
+  .addDecorator(Padding)
+  .add("Standard Tabs", () => (
+    <Tabs>
+      <Tab />
+      <Tab isSelected />
+      <Tab />
+    </Tabs>
+  ))
