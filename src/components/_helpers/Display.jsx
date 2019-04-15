da3e4c7acc87ad46896a9d9b3c3a0020aa313d0b
Greg Person <gperson@digmap.com>
2019-04-15 10:28:28 -0700

    Variables and Helpers

--- src/components/_helpers/Display.jsx
+++ src/components/_helpers/Display.jsx
@@ -1,20 +1,17 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import styled from "styled-components"
 
 const centerStyles = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   height: "100vh"
-};
-const Center = ({ children }) => <div style={centerStyles}>{children}</div>;
+}
+const Center = ({ children }) => <div style={centerStyles}>{children}</div>
 
 const padding = {
-  padding: "2rem",
-  justifyContent: "center",
-  display: "flex",
-  alignItems: "center"
-};
-const Padding = storyFn => <div style={centerStyles}><div style={padding}>{storyFn()}</div> </div>;
+  padding: "2rem"
+}
+const Padding = storyFn => <div style={padding}>{storyFn()}</div>
 
-export { Center, Padding };
+export { Center, Padding }
