79035e748cfe12b405536dff270261fe86406e6d
Elizabeth Gallagher <EGallagher@digmap.com>
2019-04-26 15:28:01 -0700

    update code

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,26 +1,26 @@
-// import React from "react";
-// import { storiesOf } from "@storybook/react";
-// import { Padding } from "helpers/Display";
-// import { withInfo } from "@storybook/addon-info";
-// import Button, { StyledButton as NewButton } from "atoms/Button";
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Padding } from "helpers/Display";
+import { withInfo } from "@storybook/addon-info";
+import Button, { StyledButton as NewButton } from "atoms/Button";
 
-// // Buttons
-// storiesOf("Atoms|Button", module)
-//   .addDecorator(withInfo)
-//   .addDecorator(Padding)
-//   .add("Standard Button", () => <Button buttonLabel="Standard Button" />)
-//   .add("Primary Button", () => (
-//     <Button buttonLabel="Primary Button" isPrimary={true} />
-//   ))
-//   .add("Icon Button", () => (
-//     <Button buttonLabel="Icon Button" icon={["fas", "user"]} />
-//   ))
-//   .add("Square Button", () => (
-//     <Button buttonLabel="Squared Button" isSquare={true} />
-//   ))
-//   .add("Floating Button", () => (
-//     <Button buttonLabel="Floating Button" isFloating={true} />
-//   ))
-//   .add("Disabled Button", () => (
-//     <Button buttonLabel="Disabled Button" isDisabled={true} />
-//   ));
+// Buttons
+storiesOf("Atoms|Button", module)
+  .addDecorator(withInfo)
+  .addDecorator(Padding)
+  .add("Standard Button", () => <Button buttonLabel="Standard Button" />)
+  .add("Primary Button", () => (
+    <Button buttonLabel="Primary Button" isPrimary={true} />
+  ))
+  .add("Icon Button", () => (
+    <Button buttonLabel="Icon Button" icon={["fas", "user"]} />
+  ))
+  .add("Square Button", () => (
+    <Button buttonLabel="Squared Button" isSquare={true} />
+  ))
+  .add("Floating Button", () => (
+    <Button buttonLabel="Floating Button" isFloating={true} />
+  ))
+  .add("Disabled Button", () => (
+    <Button buttonLabel="Disabled Button" isDisabled={true} />
+  ));
