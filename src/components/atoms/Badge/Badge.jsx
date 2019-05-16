2f91ea300c828ca07e64dd88914117d8287f0f75
Greg Person <gperson@digmap.com>
2019-05-16 16:37:45 -0700

    Badge - Updated the conditional syntax of the switch function

--- src/components/atoms/Badge/Badge.jsx
+++ src/components/atoms/Badge/Badge.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import PropTypes from "prop-types"
-import styled, { css } from "styled-components"
-import { colors } from "Variables"
-import Icon from "atoms/Icon"
+import React from "react";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
+import { colors } from "Variables";
+import Icon from "atoms/Icon";
 
 const BadgeContainer = styled.div`
   position: absolute;
@@ -20,88 +20,97 @@ const BadgeContainer = styled.div`
   bottom: ${props => props.badgeBottom || ""};
   left: ${props => props.badgeLeft || ""};
   transform: ${props => props.badgeTransform || ""};
-`
+`;
 
 const BadgeLabel = styled.span`
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
   margin: 0;
-`
+`;
 
-const BadgeIcon = styled(Icon)``
+const BadgeIcon = styled(Icon)``;
 
 function Badge({ id, as, label, icon, type, size, position, style, ...props }) {
-  let badgeColor = colors.alert
-  let badgeText = colors.white
-  let badgeSize = ".6em"
-  let badgePadding = "0.45em .8em"
-  let badgeLeft = "100%"
-  let badgeBottom = "100%"
-  let badgeTransform = "translate(-90%, 50%)"
+  let badgeColor = colors.alert;
+  let badgeText = colors.white;
+  let badgeSize = ".6em";
+  let badgePadding = "0.45em .8em";
+  let badgeLeft = "100%";
+  let badgeBottom = "100%";
+  let badgeTransform = "translate(-90%, 50%)";
   switch (type) {
     case "info":
-      as = "a"
-      icon
-        ? (badgeText = colors.anchor) &&
-          (badgeColor = "none") &&
-          (badgeSize = "1.25em") &&
-          (badgePadding = ".25em")
-        : (badgeColor = colors.anchor)
-      break
+      as = "a";
+      if (icon) {
+        badgeColor = "none";
+        badgeSize = "1.25em";
+        badgePadding = ".25em";
+        badgeText = colors.anchor;
+      } else {
+        badgeColor = colors.anchor;
+      }
+      break;
     case "success":
-      icon
-        ? (badgeColor = "none") &&
-          (badgeText = colors.success) &&
-          (badgeSize = "1.25em") &&
-          (badgePadding = ".25em")
-        : (badgeColor = colors.success)
-      break
+      if (icon) {
+        badgeColor = "none";
+        badgeSize = "1.25em";
+        badgePadding = ".25em";
+        badgeText = colors.success;
+      } else {
+        badgeColor = colors.success;
+      }
+      break;
     case "warning":
-      icon
-        ? (badgeColor = "none") &&
-          (badgeText = colors.warning) &&
-          (badgeSize = "1.25em") &&
-          (badgePadding = ".25em")
-        : (badgeColor = colors.warning)
-      break
+      if (icon) {
+        badgeColor = "none";
+        badgeSize = "1.25em";
+        badgePadding = ".25em";
+        badgeText = colors.warning;
+      } else {
+        badgeColor = colors.warning;
+      }
+      break;
     case "alert":
-      icon
-        ? (badgeColor = "none") &&
-          (badgeText = colors.alert) &&
-          (badgeSize = "1.25em") &&
-          (badgePadding = ".25em")
-        : (badgeColor = colors.alert)
-      break
+      if (icon) {
+        badgeColor = "none";
+        badgeSize = "1.25em";
+        badgePadding = ".25em";
+        badgeText = colors.alert;
+      } else {
+        badgeColor = colors.alert;
+      }
+      break;
     case "dark":
-      icon
-        ? (badgeColor = "none") &&
-          (badgeText = colors.grey_80) &&
-          (badgeSize = "1.25em") &&
-          (badgePadding = ".25em")
-        : (badgeColor = colors.grey_80)
-      break
+      if (icon) {
+        badgeColor = "none";
+        badgeSize = "1.25em";
+        badgePadding = ".25em";
+        badgeText = colors.grey_80;
+      } else {
+        badgeColor = colors.grey_80;
+      }
+      break;
     default:
-      ""
+      break;
   }
   switch (position) {
     case "topLeft":
-      badgeLeft = "0"
-      badgeTransform = "translate(-10%, 50%)"
-      break
+      badgeLeft = "0";
+      badgeTransform = "translate(-10%, 50%)";
+      break;
     case "topRight":
-      ""
-      break
+      break;
     case "bottomRight":
-      badgeBottom = "0"
-      break
+      badgeBottom = "0";
+      break;
     case "bottomLeft":
-      badgeBottom = "0"
-      badgeLeft = "0"
-      badgeTransform = "translate(-10%, 50%)"
-      break
+      badgeBottom = "0";
+      badgeLeft = "0";
+      badgeTransform = "translate(-10%, 50%)";
+      break;
     default:
-      ""
+      break;
   }
   return (
     <BadgeContainer
@@ -124,13 +133,13 @@ function Badge({ id, as, label, icon, type, size, position, style, ...props }) {
         <BadgeLabel>{label}</BadgeLabel>
       )}
     </BadgeContainer>
-  )
+  );
 }
 
 Badge.defaultProps = {
   type: "alert",
   position: "topRight"
-}
+};
 
 Badge.propTypes = {
   id: PropTypes.string,
@@ -155,6 +164,6 @@ Badge.propTypes = {
     "bottomLeft"
   ]),
   style: PropTypes.string
-}
+};
 
-export { Badge as default }
+export { Badge as default };
