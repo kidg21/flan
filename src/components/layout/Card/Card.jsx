5f010e0e5ff39a327fbab567571fbe378785fe78
Greg Person <gperson@digmap.com>
2019-05-28 17:41:33 -0700

    Card - Added 'relative' positioning so that child components with 'absolute' positioning will display properly

--- src/components/layout/Card/Card.jsx
+++ src/components/layout/Card/Card.jsx
@@ -1,17 +1,17 @@
-import React from "react"
-import styled, { css } from "styled-components"
-import { fonts, colors, shadows } from "Variables"
-import Grid from "../../_helpers/Grid"
-import { PlaceholderText } from "../../_helpers/Placeholders"
+import React from "react";
+import styled, { css } from "styled-components";
+import { fonts, colors, shadows } from "Variables";
+import Grid from "helpers/Grid";
+import { PlaceholderText } from "helpers/Placeholders";
 
 const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: none;
+  position: relative;
   width: 100%;
   background: ${colors.white};
   filter: ${shadows.cardShadow};
-  /* box-shadow: ${shadows.cardShadow}; */
   /* Square off rounded edges of any direct children of Cards */
   > * {
     border-radius: 0;
@@ -24,13 +24,9 @@ const CardWrapper = styled.div`
       padding: 2rem;
     }
   }
-`
+`;
 
-// const CardList = styled.ul`
 const CardList = styled(Grid)`
-  /* display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr)); */
   padding: 1rem;
   ${CardWrapper} {
     border-radius: 5px;
@@ -42,12 +38,10 @@ const CardList = styled(Grid)`
       content: "{ CardList } \00000A 'Displays a grid of Cards in columns'";
     }
   }
-`
+`;
 
-function Card({ id, children}) {
-  return <CardWrapper>{children}</CardWrapper>
+function Card({ id, children }) {
+  return <CardWrapper>{children}</CardWrapper>;
 }
 
-
-
-export { Card as default, CardList }
+export { Card as default, CardList };
