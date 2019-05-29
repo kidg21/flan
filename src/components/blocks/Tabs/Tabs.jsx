a5dc28378b6616e9ece417e07381d6ba29c9b07b
Greg Person <gperson@digmap.com>
2019-05-29 13:41:13 -0700

    Tabs - Refactored the switch cases

--- src/components/blocks/Tabs/Tabs.jsx
+++ src/components/blocks/Tabs/Tabs.jsx
@@ -23,7 +23,7 @@ const TabsWrapper = styled.section`
   }
 `;
 
-function Tabs({ id, children, display, align, isFloating, style }) {
+function Tabs({ id, children, columns, align, isFloating, style }) {
   let setColumns;
   let setPosition;
   let setWidth;
@@ -31,31 +31,24 @@ function Tabs({ id, children, display, align, isFloating, style }) {
   let setOrientation;
   let alignRight;
   let alignBottom;
-  switch (display) {
+  switch (columns) {
     case "wrap":
       setColumns = "repeat(auto-fit, minmax(8rem, 1fr))";
       break;
-    case "col_1":
+    case "1":
       setColumns = "repeat(1, 1fr)";
       break;
-    case "col_2":
+    case "2":
       setColumns = "repeat(2, 1fr)";
       break;
-    case "col_3":
+    case "3":
       setColumns = "repeat(3, 1fr)";
       break;
-    case "col_4":
+    case "4":
       setColumns = "repeat(4, 1fr)";
       break;
-    case "col_5":
+    case "5":
       setColumns = "repeat(5, 1fr)";
-      break;
-    case "vertical":
-      setColumns = "none";
-      setPosition = "absolute";
-      setWidth = "min-content";
-      setHeight = "100%";
-      setOrientation = "flex";
       break;
     default:
       break;
@@ -65,8 +58,20 @@ function Tabs({ id, children, display, align, isFloating, style }) {
       setPosition = "absolute";
       alignBottom = "0";
       break;
-    case "right":
+    case "left":
+      setColumns = "none";
       setPosition = "absolute";
+      setWidth = "min-content";
+      setHeight = "100%";
+      setOrientation = "flex";
+      alignBottom = "0";
+      break;
+    case "right":
+      setColumns = "none";
+      setPosition = "absolute";
+      setWidth = "min-content";
+      setHeight = "100%";
+      setOrientation = "flex";
       alignRight = "0";
       break;
     default:
@@ -121,17 +126,8 @@ function Tab({ id, icon, tabLabel, onClick, isSelected, isDisabled }) {
 Tabs.propTypes = {
   id: PropTypes.string,
   children: PropTypes.node.isRequired,
-  display: PropTypes.oneOf([
-    "default",
-    "wrap",
-    "col_1",
-    "col_2",
-    "col_3",
-    "col_4",
-    "col_5",
-    "vertical"
-  ]),
-  align: PropTypes.oneOf(["bottom", "right"]),
+  columns: PropTypes.oneOf(["default", "wrap", "1", "2", "3", "4", "5"]),
+  align: PropTypes.oneOf(["bottom", "left", "right"]),
   isFloating: PropTypes.bool,
   style: PropTypes.string
 };
