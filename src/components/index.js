11fdea3f7ab5911eeb53b24c11cfb59e9115bf86
Greg Person <gperson@digmap.com>
2019-05-31 18:26:28 -0700

    Modal (new) - Began work on a modal layout container

--- src/components/index.js
+++ src/components/index.js
@@ -43,5 +43,6 @@ export {
   InputGroup
 } from "./layout/Form";
 export { default as Layout } from "./layout/Layout";
+export { default as Modal } from "./layout/Modal";
 export { default as Page } from "./layout/Page";
 export { default as Panel, PanelSection } from "./layout/Panel";
