18b475851eda95f1381837520446b1a744c98b77
Greg Person <gperson@digmap.com>
2019-06-07 16:02:28 -0700

    - Removed logic from both component and story - Moved configurations into a 'type' switch statement - Created a 'CheckboxGroup' component to handle layout - Added a 'columns' switch for group configuration - Updated stories and added knobs

--- src/components/atoms/Checkbox/Checkbox.jsx
+++ src/components/atoms/Checkbox/Checkbox.jsx
@@ -1,28 +1,34 @@
-import React from "react"
-import styled from "styled-components"
-import PropTypes from "prop-types"
-import { colors} from "Variables"
+import React, { useState } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { colors } from "Variables";
+
+const CheckboxWrapper = styled.section`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: ${props =>
+    props.setColumns || "repeat(auto-fit, minmax(8rem, 1fr))"};
+  margin-bottom: 1rem;
+`;
 
 const CheckboxContainer = styled.div`
   display: grid;
   grid-template-columns: auto 1fr;
   grid-gap: 0.75rem;
   align-items: inherit;
-  color: ${props => (props.error ? colors.alert : "")};
+  color: ${props => props.checkboxColor || ""};
   &[disabled],
   &[readonly] {
     cursor: not-allowed;
     pointer-events: none;
     user-select: none;
-    color: ${colors.grey_40};
   }
-`
+`;
 
 const CheckboxInput = styled.input.attrs({ type: "checkbox" })`
   border: 1px solid;
-  border-color: ${props => (props.error ? colors.alert_light : colors.grey_40)};
-  background-color: ${props =>
-    props.disabled ? colors.grey_20 : colors.white};
+  background-color: ${props => props.fillColor || colors.white};
+  border-color: ${props => props.borderColor || colors.grey_40};
   width: 1rem;
   height: 1rem;
   border-radius: 2px;
@@ -30,14 +36,13 @@ const CheckboxInput = styled.input.attrs({ type: "checkbox" })`
   -webkit-appearance: none;
   &:checked {
     background-color: ${props =>
-      props.error ? colors.alert_tint : colors.success_tint};
-    border-color: ${props =>
-      props.error ? colors.alert_light : colors.success};
+      props.fillColorChecked || colors.success_light};
+    border-color: ${props => props.borderColorChecked || colors.success};
   }
   &:focus {
     outline: none;
   }
-`
+`;
 
 const CheckboxLabel = styled.label`
   user-select: none;
@@ -46,27 +51,85 @@ const CheckboxLabel = styled.label`
   font-weight: 400;
   line-height: 1.2;
   cursor: pointer;
-`
+`;
 
-function Checkbox({ id, label, error, disabled }) {
+function CheckboxGroup({ id, columns, onChange, children }) {
+  let setColumns;
+  switch (columns) {
+    case "1":
+      setColumns = "repeat(1, 1fr)";
+      break;
+    case "2":
+      setColumns = "repeat(2, 1fr)";
+      break;
+    case "3":
+      setColumns = "repeat(3, 1fr)";
+      break;
+    case "4":
+      setColumns = "repeat(4, 1fr)";
+      break;
+    default:
+      break;
+  }
+  return (
+    <CheckboxWrapper id={id} onChange={onChange} setColumns={setColumns}>
+      {children}
+    </CheckboxWrapper>
+  );
+}
+
+function Checkbox({ id, label, type, checked, error, isDisabled }) {
+  let checkboxColor;
+  let fillColor;
+  let borderColor;
+  let fillColorChecked;
+  let borderColorChecked;
+  switch (type) {
+    case "error":
+      checkboxColor = colors.alert;
+      fillColor = colors.alert_tint;
+      borderColor = checkboxColor;
+      fillColorChecked = colors.alert_tint;
+      borderColorChecked = colors.alert;
+      break;
+    case "disabled":
+      checkboxColor = colors.grey_40;
+      fillColor = colors.grey_20;
+      borderColor = checkboxColor;
+      isDisabled = true;
+      break;
+    default:
+      break;
+  }
   return (
     <CheckboxContainer
-      disabled={disabled}
+      checkboxColor={checkboxColor}
       error={error}
+      disabled={isDisabled}
     >
       <CheckboxInput
         id={id}
-        disabled={disabled}
-        error={error}
+        checked={checked}
+        fillColor={fillColor}
+        borderColor={borderColor}
+        fillColorChecked={fillColorChecked}
+        borderColorChecked={borderColorChecked}
       />
       <CheckboxLabel htmlFor={id}>{label}</CheckboxLabel>
     </CheckboxContainer>
-  )
+  );
 }
+
+CheckboxGroup.propTypes = {
+  id: PropTypes.string,
+  onChange: PropTypes.func,
+  columns: PropTypes.oneOf(["wrap (default)", "1", "2"])
+};
 
 Checkbox.propTypes = {
   id: PropTypes.string,
-  label: PropTypes.string.isRequired
-}
+  label: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["error", "disabled"])
+};
 
-export { Checkbox as default }
+export { Checkbox as default, CheckboxGroup };
