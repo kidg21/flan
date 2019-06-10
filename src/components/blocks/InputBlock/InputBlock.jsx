185bd4c8f29054dcecb3e540ca55449d9c436c64
Greg Person <gperson@digmap.com>
2019-06-10 14:54:50 -0700

    - Changed current 'Input' to an 'input Block' component - Began simplifying original "input'

---
+++ src/components/blocks/InputBlock/InputBlock.jsx
@@ -0,0 +1,254 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import GlobalStyles from "GlobalStyles";
+import PropTypes from "prop-types";
+import { colors, shadows } from "Variables";
+import { InputLabel, HelpText, ErrorText } from "layout/Form";
+import SelectMenu from "atoms/SelectMenu";
+import Button from "atoms/Button";
+
+const TextInputContainer = styled.div`
+  display: grid;
+  grid-template-columns: ${props =>
+    /* 3 Inputs */
+    props.threeInputs
+      ? "repeat(3, 1fr)"
+      : /* 2 Inputs */
+      props.twoInputs
+      ? "repeat(2, 1fr)"
+      : /* Prefix Label (conditionals) */
+      props.prefix
+      ? props.postfix || props.buttonLabel
+        ? "minmax(auto, auto) minmax(auto, 3fr) minmax(auto, auto)"
+        : props.postSelect
+        ? "minmax(auto, auto) minmax(auto, 3fr) minmax(auto, 2fr)"
+        : "minmax(auto, auto) minmax(auto, 3fr)"
+      : /* Postfix Select (conditionals) */
+      props.preSelect
+      ? props.postfix || props.buttonLabel
+        ? "minmax(auto, 2fr) minmax(auto, 3fr) minmax(auto, auto)"
+        : props.postSelect
+        ? "minmax(auto, 2fr) minmax(auto, 3fr) minmax(auto, 2fr)"
+        : "minmax(auto, 2fr) minmax(auto, 3fr)"
+      : /* Postfix Label */
+      props.postfix || props.buttonLabel
+      ? "minmax(auto, 3fr) minmax(auto, auto)"
+      : /* Postfix Select */
+      props.postSelect
+      ? "minmax(auto, 3fr) minmax(auto, 2fr)"
+      : /* Single Input (default) */
+        "repeat(1, 1fr)"};
+  grid-gap: ${props => (props.slider ? "0.15rem" : "0.35rem")};
+  align-content: flex-start;
+  color: ${props =>
+    props.error ? colors.alert : props.disabled ? colors.grey_40 : ""};
+`;
+
+const PrePostLabel = styled.label`
+  display: flex;
+  justify-content: center;
+  border-radius: 5px;
+  align-items: center;
+  font-weight: bold;
+  letter-spacing: 2px;
+  text-transform: lowercase;
+  color: ${colors.grey_60};
+  background-color: ${colors.grey_light};
+  border: 1px solid ${colors.grey_20};
+  border-bottom: 1px solid ${colors.grey_20};
+  border-radius: 4px;
+  padding: 0.25rem 1rem;
+  white-space: nowrap;
+`;
+
+const TextInput = styled.input`
+  border: 1px solid ${colors.grey_20};
+  border-bottom: 1px solid ${colors.grey_20};
+  border-color: ${props => (props.error ? colors.alert : "")};
+  border-radius: ${props => (props.isRound ? "10rem !important" : "")};
+  background-color: ${props => (props.error ? colors.alert_tint : "")};
+  caret-color: ${props => (props.error ? colors.alert : "")};
+  min-height: 2.75rem;
+  padding: ${props => (props.isRound ? "0.75rem 1rem" : "0.5rem 0.75rem")};
+
+  ::placeholder {
+    color: ${props => (props.error ? colors.alert : "")};
+  }
+  &:hover {
+    border: 1px solid ${colors.grey_40};
+    border-color: ${props => (props.error ? colors.alert : "")};
+    }
+  }
+  &:focus {
+    background-color: ${props => (props.error ? colors.alert_tint : "")};
+    border-color: ${props => (props.error ? colors.alert : colors.success)};
+    ::placeholder {
+      color: ${props => (props.error ? colors.alert : colors.grey_60)};
+    }
+    ::selection {
+      background-color: ${props => (props.error ? colors.alert : "")};
+      
+    }
+  }
+`;
+
+function InputBlock({
+  id,
+  type,
+  pattern,
+  value,
+  inputLabel, // Label prop
+  placeholder,
+  helpText,
+  disabled,
+  isRequired,
+  isRound,
+  twoInputs,
+  id_2,
+  type_2,
+  value_2,
+  pattern_2,
+  placeholder_2,
+  threeInputs,
+  id_3,
+  type_3,
+  value_3,
+  pattern_3,
+  placeholder_3,
+  prefix,
+  postfix,
+  preSelect,
+  postSelect,
+  buttonLabel,
+  error,
+  errorText,
+  style
+}) {
+  return (
+    <TextInputContainer
+      id={id}
+      isRequired={isRequired}
+      disabled={disabled} // input attribute
+      error={error}
+      prefix={prefix}
+      postfix={postfix}
+      preSelect={preSelect}
+      postSelect={postSelect}
+      buttonLabel={buttonLabel}
+      twoInputs={twoInputs} // 2 inputs in a row
+      threeInputs={threeInputs} // 3 inputs in a row
+      style={style}
+    >
+      {/* Input Label */}
+      <InputLabel inputLabel={inputLabel} isRequired={isRequired} />
+      {/* Prefix Label (conditional) */}
+      {prefix ? <PrePostLabel>{prefix}</PrePostLabel> : null}
+      {/* Prefix Select Menu (conditional) */}
+      {preSelect ? (
+        <SelectMenu
+          displayInline={true} // Grid Override
+          inputLabel={null}
+          name="Choose"
+          isClearable={false}
+          options={preSelect}
+          defaultValue={preSelect[0]}
+        />
+      ) : null}
+      <TextInput
+        id={id} // input attribute
+        name={id} // input attribute
+        type={type} // input attribute
+        value={value} // input attribute
+        placeholder={placeholder} // input attribute
+        pattern={pattern} // input attribute
+        disabled={disabled} // input attribute
+        error={error}
+        isRound={isRound}
+      />
+      {/* Column 2 (conditional) */}
+      {twoInputs || threeInputs ? (
+        <TextInput
+          id={id_2} // input attribute
+          name={id_2} // input attribute
+          type={type_2} // input attribute
+          value={value_2} // input attribute
+          placeholder={placeholder_2} // input attribute
+          pattern={pattern_2} // input attribute
+          disabled={disabled} // input attribute
+          error={error}
+          isRound={isRound}
+        />
+      ) : null}
+      {/* Column 3 (conditional) */}
+      {threeInputs ? (
+        <TextInput
+          id={id_3} // input attribute
+          name={id_3} // input attribute
+          type={type_3} // input attribute
+          value={value_3} // input attribute
+          placeholder={placeholder_3} // input attribute
+          pattern={pattern_3} // input attribute
+          disabled={disabled} // input attribute
+          error={error}
+          isRound={isRound}
+        />
+      ) : null}
+      {/* Postfix (conditional) */}
+      {postfix ? <PrePostLabel>{postfix}</PrePostLabel> : null}
+      {/* Postfix Button (conditional) */}
+      {buttonLabel ? (
+        <Button buttonLabel={buttonLabel} isPrimary={true} />
+      ) : null}
+      {/* Postfix Select Menu (conditional) */}
+      {postSelect ? (
+        <SelectMenu
+          displayInline={true} // Grid Override
+          inputLabel={null}
+          name="Choose"
+          isClearable={false}
+          options={postSelect}
+          defaultValue={postSelect[0]}
+        />
+      ) : null}
+      {/* Help Text */}
+      {helpText ? <HelpText helpText={helpText} /> : null}
+      {/* Error Message (required) */}
+      {error ? <ErrorText errorText={errorText} /> : null}
+    </TextInputContainer>
+  );
+}
+
+InputBlock.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  pattern: PropTypes.string,
+  value: PropTypes.string,
+  inputLabel: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  helpText: PropTypes.string,
+  isRequired: PropTypes.bool,
+  disabled: PropTypes.bool,
+  isRound: PropTypes.bool,
+  twoInputs: PropTypes.bool,
+  id_2: PropTypes.string,
+  type_2: PropTypes.string,
+  value_2: PropTypes.string,
+  pattern_2: PropTypes.string,
+  placeholder_2: PropTypes.string,
+  threeInputs: PropTypes.bool,
+  id_3: PropTypes.string,
+  type_3: PropTypes.string,
+  value_3: PropTypes.string,
+  pattern_3: PropTypes.string,
+  placeholder_3: PropTypes.string,
+  prefix: PropTypes.string,
+  postfix: PropTypes.string,
+  preSelect: PropTypes.array,
+  postSelect: PropTypes.array,
+  buttonLabel: PropTypes.string,
+  error: PropTypes.bool,
+  errorText: PropTypes.string,
+  style: PropTypes.string
+};
+
+export { InputBlock as default };
---
+++ src/components/blocks/InputBlock/InputBlock.stories.js
@@ -0,0 +1,367 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Padding } from "helpers/Display";
+import { withInfo } from "@storybook/addon-info";
+import {
+  withKnobs,
+  text,
+  boolean,
+  radios,
+  select,
+  number
+} from "@storybook/addon-knobs";
+import Panel, { PanelSection } from "../../layout/Panel/Panel";
+import Form, { Section, SectionName } from "../../layout/Form/Form";
+import Icon from "atoms/Icon";
+import Input from "atoms/Input";
+import InputBlock from "blocks/InputBlock";
+import InputBlockNotes from "./InputBlock.md";
+
+// Input ( Standard )
+storiesOf("Blocks|InputBlock", module)
+  .addParameters({
+    info: {
+      text:
+        "Input Blocks allow users to enter text into a UI. They typically appear in forms and dialogs"
+    },
+    notes: {
+      markdown: InputBlockNotes
+    }
+  })
+  .addDecorator(Padding)
+  .addDecorator(withKnobs)
+  .add(
+    "Documentation",
+    withInfo()(() => (
+      <Input
+        type={radios(
+          "Type",
+          {
+            Text: "text",
+            Number: "number"
+          },
+          "text"
+        )}
+        inputLabel={text("Input Label", "Input Label")}
+        isRequired={boolean("Required", false)}
+        placeholder={text("Placeholder Text", "I am just keeping things warm")}
+        helpText={text("Help Text", "Have you been helped yet?")}
+        disabled={boolean("Disabled", false)}
+        error={boolean("Error", false)}
+        errorMessage={text(
+          "Error Text",
+          "Stay with my, buddy...we can fix this!"
+        )}
+        prefix={text("Prefix", "")}
+        postfix={text("Postfix", "")}
+        buttonLabel={text("Button Label", "")}
+        twoInputs={boolean("2 Inputs", false)}
+        threeInputs={boolean("3 Inputs", false)}
+      />
+    ))
+  )
+  // Text ( Standard )
+  .add("Input ( Standard )", () => (
+    <InputBlock
+      name="Say My Name!"
+      value="Text Input"
+      inputLabel="Text ( Standard )"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Number ( Number )
+  .add("Input ( Number )", () => (
+    <InputBlock
+      type="number"
+      inputLabel="Number ( Standard )"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Required )
+  .add("Input ( Required )", () => (
+    <InputBlock
+      inputLabel="Input ( Required )"
+      isRequired={true}
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Disabled )
+  .add("Input ( Disabled )", () => (
+    <InputBlock
+      inputLabel="Input ( Disabled )"
+      disabled={true}
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Error )
+  .add("Input ( Error )", () => (
+    <InputBlock
+      inputLabel="Input ( Error )"
+      helpText="This help text has been passed through a prop!"
+      error={true}
+      errorMessage="This error text has been passed through a prop!"
+    />
+  ))
+  // Input ( Round )
+  .add("Input ( Round )", () => (
+    <InputBlock
+      inputLabel="Input ( Round )"
+      helpText="This help text has been passed through a prop!"
+      isRound={true}
+    />
+  ))
+  // Input ( 2 Inputs )
+  .add("Input ( 2 Inputs )", () => (
+    <InputBlock
+      inputLabel="Input ( 2 Inputs )"
+      twoInputs={true}
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( 3 Inputs )
+  .add("Input ( 3 Inputs )", () => (
+    <InputBlock
+      inputLabel="Input ( 3 Inputs )"
+      threeInputs={true}
+      placeholder_2="Two!!"
+      placeholder_3="Three!!!"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Prefix )
+  .add("Input ( Prefix Label )", () => (
+    <InputBlock
+      inputLabel="Input ( Prefix )"
+      prefix="http://"
+      placeholder="This help text has been passed through a prop!"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Prefix Icon )
+  .add("Input ( Prefix Icon )", () => (
+    <InputBlock
+      inputLabel="Input ( Prefix Icon )"
+      prefix={<Icon icon={["fas", "user"]} size="lg" fixedWidth />}
+      placeholder="This help text has been passed through a prop!"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Pre-Select )
+  .add("Input ( Pre-Select )", () => (
+    <InputBlock
+      inputLabel="Input ( Pre-Select )"
+      preSelect={[
+        { value: "sir", label: "Sir" },
+        { value: "madam", label: "Madam" },
+        { value: "my lord", label: "My Lord" }
+      ]}
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Postfix )
+  .add("Input ( Postfix Label )", () => (
+    <InputBlock
+      inputLabel="Input ( Postfix )"
+      postfix=".com"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Post-Select )
+  .add("Input ( Post-Select )", () => (
+    <InputBlock
+      inputLabel="Input ( Post-Select )"
+      postSelect={[
+        { value: "com", label: ".com" },
+        { value: "org", label: ".org" },
+        { value: "gov", label: ".gov" }
+      ]}
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Post-Button )
+  .add("Input ( Post-Button )", () => (
+    <InputBlock
+      inputLabel="Input ( Post-Button )"
+      postButton={true}
+      buttonLabel="Upload"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Prefix / Postfix )
+  .add("Input ( Prefix / Postfix )", () => (
+    <InputBlock
+      inputLabel="Input ( Prefix / Postfix )"
+      prefix="http://"
+      postfix=".com"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Prefix / Post-Select )
+  .add("Input ( Prefix / Post-Select )", () => (
+    <InputBlock
+      inputLabel="Input ( Prefix / Post-Select )"
+      prefix="http://"
+      postSelect={[
+        { value: "com", label: ".com" },
+        { value: "org", label: ".org" },
+        { value: "gov", label: ".gov" }
+      ]}
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Pre-Select / Postfix )
+  .add("Input ( Pre-Select / Postfix )", () => (
+    <InputBlock
+      inputLabel="Input ( Pre-Select / Postfix )"
+      preSelect={[
+        { value: "sir", label: "Sir" },
+        { value: "madam", label: "Madam" },
+        { value: "my lord", label: "My Lord" }
+      ]}
+      postfix=".com"
+      helpText="This help text has been passed through a prop!"
+    />
+  ))
+  // Input ( Pre / Post-Select )
+  .add("Input ( Pre / Post-Select )", () => (
+    <InputBlock
+      inputLabel="Input ( Pre / Post-Select )"
+      preSelect={[
+        { value: "sir", label: "Sir" },
+        { value: "madam", label: "Madam" },
+        { value: "my lord", label: "My Lord" }
+      ]}
+      postSelect={[
+        { value: "com", label: ".com" },
+        { value: "org", label: ".org" },
+        { value: "gov", label: ".gov" }
+      ]}
+      helpText="This help text has been passed through a prop!"
+    />
+  ));
+// Input ( The Input Family )
+storiesOf("Blocks|InputBlock", module).add("The Input Block Family", props => (
+  <Panel>
+    <PanelSection body>
+      <Form>
+        <Section>
+          <SectionName>The Input Family</SectionName>
+          <InputBlock
+            inputLabel="Input ( Standard )"
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Required )"
+            isRequired={true}
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Disabled )"
+            disabled={true}
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Error )"
+            helpText="This help text has been passed through a prop!"
+            error={true}
+            errorMessage="This error text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( 2 Inputs )"
+            twoInputs={true}
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( 3 Inputs )"
+            threeInputs={true}
+            helpText="This help text has been passed through a prop!"
+            placeholder_2="Two!!"
+            placeholder_3="Three!!!"
+          />
+          <InputBlock
+            inputLabel="Input ( Prefix Label )"
+            prefix="http://"
+            placeholder="This help text has been passed through a prop!"
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Prefix Icon )"
+            prefix={<Icon icon={["fas", "user"]} size="lg" fixedWidth />}
+            placeholder="This help text has been passed through a prop!"
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Pre-Select )"
+            preSelect={[
+              { value: "sir", label: "Sir" },
+              { value: "madam", label: "Madam" },
+              { value: "my lord", label: "My Lord" }
+            ]}
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Postfix Label )"
+            postfix=".com"
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Post-Select )"
+            postSelect={[
+              { value: "com", label: ".com" },
+              { value: "org", label: ".org" },
+              { value: "gov", label: ".gov" }
+            ]}
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Post-Button )"
+            postButton={true}
+            buttonLabel="Upload"
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Prefix / Postfix )"
+            prefix="http://"
+            postfix=".com"
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Prefix / Post-Select )"
+            prefix="http://"
+            postSelect={[
+              { value: "com", label: ".com" },
+              { value: "org", label: ".org" },
+              { value: "gov", label: ".gov" }
+            ]}
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Pre-Select / Postfix )"
+            preSelect={[
+              { value: "sir", label: "Sir" },
+              { value: "madam", label: "Madam" },
+              { value: "my lord", label: "My Lord" }
+            ]}
+            postfix=".com"
+            helpText="This help text has been passed through a prop!"
+          />
+          <InputBlock
+            inputLabel="Input ( Pre / Post-Select )"
+            preSelect={[
+              { value: "sir", label: "Sir" },
+              { value: "madam", label: "Madam" },
+              { value: "my lord", label: "My Lord" }
+            ]}
+            postSelect={[
+              { value: "com", label: ".com" },
+              { value: "org", label: ".org" },
+              { value: "gov", label: ".gov" }
+            ]}
+            helpText="This help text has been passed through a prop!"
+          />
+        </Section>
+      </Form>
+    </PanelSection>
+  </Panel>
+));
