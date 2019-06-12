78e208ce9d10221ef08044f45e8875530ab5418c
Greg Person <gperson@digmap.com>
2019-06-11 17:38:56 -0700

    - Moved the 'status' type of Modal into its own "notification' component

--- src/components/layout/Notification/Notification.jsx
+++ src/components/layout/Notification/Notification.jsx
@@ -3,8 +3,6 @@ import styled, { css, keyframes } from "styled-components";
 import { fonts, colors, shadows, screen } from "Variables";
 import { PlaceholderText } from "helpers/Placeholders.jsx";
 import PropTypes from "prop-types";
-import Icon from "atoms/Icon";
-import Mapbox from "layout/Map";
 
 const fadeIn = keyframes`
   from {
@@ -74,13 +72,7 @@ const ContentWrapper = styled.span`
   /* Needed for passing properties to children (animation, etc.) */
 `;
 
-const Image = styled.img`
-  width: auto;
-  max-height: 100%;
-  border: 1px solid hsl(0, 100%, 100%);
-`;
-
-const ModalContainer = styled.div`
+const NotificationContainer = styled.div`
   position: absolute;
   display: ${props => (props.visible ? "flex" : "none")};
   z-index: 1005;
@@ -91,173 +83,116 @@ const ModalContainer = styled.div`
   min-height: 100vh;
   align-items: center;
   justify-content: ${props => props.justifyContent || ""};
+  @media ${screen.medium} {
+    align-items: ${props => props.alignItems || ""};
+  }
+  @media ${screen.large} {
+    align-items: ${props => props.alignItems || ""};
+  }
   flex-direction: column;
   padding: 0.5rem;
-  background-blend-mode: multiply;
-  pointer-events: ${props => props.pointerEvents || ""};
   overflow: hidden;
-  ${ContentWrapper}, ${Image} {
+  pointer-events: none;
+  ${ContentWrapper} {
     animation-name: ${props => (props.position ? moveDown : moveUp)};
     animation-duration: 0.6s;
     transform-origin: top;
     pointer-events: initial;
+    width: 100%;
     @media ${screen.medium} {
+      width: auto;
       max-width: 50vw;
     }
     @media ${screen.large} {
+      width: auto;
       max-width: 40vw;
     }
-  }
-  ${Image} {
-    max-width: 98vw;
   }
   /* Prototype Content - displays when a Card is empty */
   &:empty {
     &:before {
       white-space: pre;
       ${PlaceholderText}
-      content: "{ Modal }";
+      content: "{ Notification }";
     }
   }
 `;
 
-const ModalBG = styled.div`
-  z-index: -1;
-  position: fixed;
-  right: 0px;
-  bottom: 0px;
-  top: 0px;
-  left: 0px;
-  background-color: hsla(34, 5%, 12%, 0.8);
-  -webkit-tap-highlight-color: transparent;
-  touch-action: none;
-  animation-name: ${props => (props.opacity ? fadeIn : fadeOut)};
-  animation-duration: 0.6s;
-`;
-
-const Close = styled.section`
-  position: absolute;
-  top: 1em;
-  right: 1em;
-  opacity: 0.5;
-  &:hover {
-    opacity: 1;
-  }
-`;
-
-const Text = styled.h5`
-  color: ${colors.white};
-  margin: 0;
-  padding: 1rem 0;
-`;
-
-function Modal({
+function Notification({
   id,
-  type,
   align,
   visible,
   onClick,
-  onClose,
   position,
   scale,
   opacity,
-  text,
-  image,
   ariaLabelledby,
   ariaDescribedby,
   children,
   style
 }) {
-  let modalType;
   let justifyContent;
-  let pointerEvents;
-  switch (type) {
-    case "text":
-      modalType = (
-        <Fragment>
-          <ModalBG onClick={onClose} opacity={opacity} />
-          <ContentWrapper>
-            <Text>{text}</Text>
-          </ContentWrapper>
-        </Fragment>
-      );
-      break;
-    case "map":
-      modalType = (
-        <Fragment>
-          <Mapbox />
-          <Close onClick={onClose}>
-            <Icon icon="times" size="lg" fixedWidth />
-          </Close>
-        </Fragment>
-      );
-      break;
-    case "image":
-      modalType = ((justifyContent = "center"),
-      (
-        <Fragment>
-          <ModalBG onClick={onClose} opacity={opacity} />
-          <Image src={image} />
-          <Close onClick={onClose}>
-            <Icon icon="times" size="lg" inverse fixedWidth />
-          </Close>
-        </Fragment>
-      ));
-      break;
-    default:
-      modalType = ((justifyContent = "center"),
-      (
-        <Fragment>
-          <ModalBG onClick={onClose} opacity={opacity} />
-          <ContentWrapper>{children}</ContentWrapper>
-        </Fragment>
-      ));
-      break;
-  }
+  let alignItems;
   switch (align) {
-    case "top":
+    case "topLeft":
+      justifyContent = "flex-start";
+      alignItems = "flex-start";
+      break;
+    case "topCenter":
       justifyContent = "flex-start";
       break;
-    case "center":
-      justifyContent = "center";
+    case "topRight":
+      justifyContent = "flex-start";
+      alignItems = "flex-end";
       break;
-    case "bottom":
+    case "bottomLeft":
       justifyContent = "flex-end";
+      alignItems = "flex-start";
+      break;
+    case "bottomCenter":
+      justifyContent = "flex-end";
+      break;
+    case "bottomRight":
+      justifyContent = "flex-end";
+      alignItems = "flex-end";
       break;
     default:
       break;
   }
   return (
-    <ModalContainer
+    <NotificationContainer
       id={id}
-      type={type}
       align={align}
       visible={visible}
-      image={image}
       position={position}
       scale={scale}
       opacity={opacity}
       aria-labelledby={ariaLabelledby}
       aria-describedby={ariaDescribedby}
-      justifyContent={justifyContent}
-      pointerEvents={pointerEvents}
       onClick={onClick}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
       children={children}
       style={style}
     >
-      {modalType}
-    </ModalContainer>
+      <ContentWrapper>{children}</ContentWrapper>
+    </NotificationContainer>
   );
 }
 
-export default Modal;
+export default Notification;
 
-Modal.propTypes = {
+Notification.propTypes = {
   id: PropTypes.string,
-  type: PropTypes.oneOf(["default", "text", "image"]),
-  align: PropTypes.oneOf(["default to type", "top", "center", "bottom"]),
+  align: PropTypes.oneOf([
+    "topLeft",
+    "topCenter",
+    "topRight",
+    "bottomLeft",
+    "bottomCenter",
+    "bottomRight"
+  ]),
   visible: PropTypes.bool,
-  image: PropTypes.string,
   position: PropTypes.oneOf(["moveUp", "moveDown"]),
   scale: PropTypes.oneOf(["scaleUp", "scaleDown"]),
   opacity: PropTypes.oneOf(["fadeIn", "fadeOut"]),
