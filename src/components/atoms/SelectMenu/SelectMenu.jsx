8486a07e36f07c232a148c2315123273d45f89b8
Greg Person <gperson@digmap.com>
2019-06-18 13:50:11 -0700

    Updated import path

--- src/components/atoms/SelectMenu/SelectMenu.jsx
+++ src/components/atoms/SelectMenu/SelectMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { fonts, colors, shadows, Lighten, Darken } from "Variables";
-import { InputLabel, HelpText, ErrorText } from "../../layout/Form/Form";
+import { InputLabel, HelpText, ErrorText } from "layout/Form";
 import Select from "react-select";
 
 const selectStyles = {
@@ -19,7 +19,9 @@ const selectStyles = {
       : isDisabled
       ? colors.grey_20
       : colors.white,
-    borderColor: isFocused ? colors.success_light + "!important" : colors.grey_20,
+    borderColor: isFocused
+      ? colors.success_light + "!important"
+      : colors.grey_20,
     borderBottomColor: colors.grey_20,
     fontWeight: "normal",
     letterSpacing: ".5px",
