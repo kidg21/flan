7e1bc29638a9f40a106bf7f7b731e00884138eab
Greg Person <gperson@digmap.com>
2019-07-22 14:12:04 -0700

    Grid - Added switch cases for setting 'columns', 'rows', and 'gap' - Set up props to to receive override values - Removed uncessary props

--- src/components/layout/Grid/Grid.jsx
+++ src/components/layout/Grid/Grid.jsx
@@ -1,157 +1,135 @@
 import React from "react";
 import styled from "styled-components";
-import { screen } from "Variables";
+import PropTypes from "prop-types";
 
 const GridWrapper = styled.section`
   display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(22rem, 1fr));
-  /* @media ${screen.medium} {
-    grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
-  }
-  @media ${screen.large} {
-    grid-template-columns: repeat(auto-fit, minmax(22rem, 1fr));
-  } */
-  /* grid-template-columns: ${props =>
-    props.col_1
-      ? "repeat(1, 1fr)"
-      : props.col_2
-      ? "repeat(2, 1fr)"
-      : props.col_3
-      ? "repeat(3, 1fr)"
-      : props.col_4
-      ? "repeat(4, 1fr)"
-      : props.col_5
-      ? "repeat(5, 1fr)"
-      : props.col_6
-      ? "repeat(6, 1fr)"
-      : props.col_7
-      ? "repeat(7, 1fr)"
-      : props.col_8
-      ? "repeat(8, 1fr)"
-      : props.col_9
-      ? "repeat(9, 1fr)"
-      : props.col_10
-      ? "repeat(10, 1fr)"
-      : props.col_11
-      ? "repeat(11, 1fr)"
-      : props.col_12
-      ? "repeat(12, 1fr)"
-      : "repeat(auto-fit, minmax(8rem, 1fr))"};
-  @media ${screen.medium} {
-    grid-template-columns: ${props =>
-      props.col_1
-        ? "repeat(1, 1fr)"
-        : props.col_2
-        ? "repeat(2, 1fr)"
-        : props.col_3
-        ? "repeat(3, 1fr)"
-        : props.col_4
-        ? "repeat(4, 1fr)"
-        : props.col_5
-        ? "repeat(5, 1fr)"
-        : props.col_6
-        ? "repeat(6, 1fr)"
-        : props.col_7
-        ? "repeat(7, 1fr)"
-        : props.col_8
-        ? "repeat(8, 1fr)"
-        : props.col_9
-        ? "repeat(9, 1fr)"
-        : props.col_10
-        ? "repeat(10, 1fr)"
-        : props.col_11
-        ? "repeat(11, 1fr)"
-        : props.col_12
-        ? "repeat(12, 1fr)"
-        : "repeat(auto-fit, minmax(14rem, 1fr))"};
-  }
-  @media ${screen.large} {
-    grid-template-columns: ${props =>
-      props.col_1
-        ? "repeat(1, 1fr)"
-        : props.col_2
-        ? "repeat(2, 1fr)"
-        : props.col_3
-        ? "repeat(3, 1fr)"
-        : props.col_4
-        ? "repeat(4, 1fr)"
-        : props.col_5
-        ? "repeat(5, 1fr)"
-        : props.col_6
-        ? "repeat(6, 1fr)"
-        : props.col_7
-        ? "repeat(7, 1fr)"
-        : props.col_8
-        ? "repeat(8, 1fr)"
-        : props.col_9
-        ? "repeat(9, 1fr)"
-        : props.col_10
-        ? "repeat(10, 1fr)"
-        : props.col_11
-        ? "repeat(11, 1fr)"
-        : props.col_12
-        ? "repeat(12, 1fr)"
-        : "repeat(auto-fit, minmax(20rem, 1fr))"};
-  } */
-  grid-column: ${props =>
-    props.alignLeft || props.alignCenter || props.alignRight
-      ? "1/-1"
-      : props.alignJustify
-      ? "none"
-      : ""};
-  justify-items: ${props =>
-    props.alignLeft
-      ? "flex-start"
-      : props.alignCenter
-      ? "center"
-      : props.alignRight
-      ? "flex-end"
-      : ""};
+  grid-gap: ${props => props.setGap || "1rem"};
+  grid-template-columns: ${props =>
+    props.setColumns || "repeat(auto-fit, minmax(22rem, 1fr))"};
+  grid-template-rows: ${props => props.setRows || "auto"};
 `;
-function Grid({
-  col_1,
-  col_2,
-  col_3,
-  col_4,
-  col_5,
-  col_6,
-  col_7,
-  col_8,
-  col_9,
-  col_10,
-  col_11,
-  col_12,
-  alignLeft,
-  alignCenter,
-  alignRight,
-  style,
-  className,
-  children
-}) {
+function Grid({ children, columns, gap, id, rows }) {
+  // 'gutter' between grid items
+  let setGap;
+  // 1-12 colums with custom override
+  let setColumns;
+  // 'auto' by default with custom override
+  // Will add specific cases as they come up
+  let setRows;
+  switch (gap) {
+    case "none":
+      setGap = "0";
+      break;
+    case "tiny":
+      setGap = ".25rem";
+      break;
+    case "small":
+      setGap = ".5rem";
+      break;
+    case "large":
+      setGap = "1.5rem";
+      break;
+    case "xlarge":
+      setGap = "2rem";
+      break;
+    case "xxlarge":
+      setGap = "3rem";
+      break;
+    default:
+    case "normal":
+      setGap = gap;
+      break;
+  }
+  switch (columns) {
+    case "1":
+      setColumns = "repeat(1, minmax(0, 1fr))";
+      break;
+    case "2":
+      setColumns = "repeat(2, minmax(0, 1fr))";
+      break;
+    case "3":
+      setColumns = "repeat(3, minmax(0, 1fr))";
+      break;
+    case "4":
+      setColumns = "repeat(4, minmax(0, 1fr))";
+      break;
+    case "5":
+      setColumns = "repeat(5, minmax(0, 1fr))";
+      break;
+    case "6":
+      setColumns = "repeat(6, minmax(0, 1fr))";
+      break;
+    case "7":
+      setColumns = "repeat(7, minmax(0, 1fr))";
+      break;
+    case "8":
+      setColumns = "repeat(8, minmax(0, 1fr))";
+      break;
+    case "9":
+      setColumns = "repeat(9, minmax(0, 1fr))";
+      break;
+    case "10":
+      setColumns = "repeat(10, minmax(0, 1fr))";
+      break;
+    case "11":
+      setColumns = "repeat(11, minmax(0, 1fr))";
+      break;
+    case "12":
+      setColumns = "repeat(12, minmax(0, 1fr))";
+      break;
+    default:
+    case "auto":
+      setColumns = columns;
+      break;
+  }
+  switch (rows) {
+    default:
+    case "auto":
+      setRows = rows;
+      break;
+  }
   return (
     <GridWrapper
-      col_1={col_1}
-      col_2={col_2}
-      col_3={col_3}
-      col_4={col_4}
-      col_5={col_5}
-      col_6={col_6}
-      col_7={col_7}
-      col_8={col_8}
-      col_9={col_9}
-      col_10={col_10}
-      col_11={col_11}
-      col_12={col_12}
-      alignLeft={alignLeft}
-      alignCenter={alignCenter}
-      alignRight={alignRight}
-      style={style}
-      className={className}
+      id={id}
+      setColumns={setColumns}
+      setGap={setGap}
+      setRows={setRows}
     >
       {children}
     </GridWrapper>
   );
 }
+
+Grid.propTypes = {
+  id: PropTypes.string,
+  setColumns: PropTypes.oneOf([
+    "default (auto)",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "[custom]",
+  ]),
+  setGap: PropTypes.oneOf([
+    "none",
+    "tiny",
+    "small",
+    "default (normal)",
+    "large",
+    "xlarge",
+    "xxlarge",
+    "[custom]",
+  ]),
+  setRows: PropTypes.oneOf(["default (auto)", "[custom]"]),
+};
 
 export { Grid as default };
--- src/components/layout/Grid/Grid.stories.js
+++ src/components/layout/Grid/Grid.stories.js
@@ -4,7 +4,7 @@ import Card from "layout/Card";
 import Grid from "layout/Grid";
 
 storiesOf("Application|Grid/", module).add("WIP", () => (
-  <Grid>
+  <Grid gap="xxlarge" columns="5fr 2fr 1fr 5fr 1fr" rows="repeat(3, 150px)">
     <Card />
     <Card />
     <Card />
