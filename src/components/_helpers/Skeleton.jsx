74a0ef7323981b6af546ba0803fc262d55177fc9
Elizabeth Gallagher <EGallagher@digmap.com>
2019-07-23 16:22:10 -0700

    Skeleton changes

--- src/components/_helpers/Skeleton.jsx
+++ src/components/_helpers/Skeleton.jsx
@@ -1,40 +1,20 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
 import { css, keyframes } from "styled-components";
 
 // Default content if component is empty - good for prototyping
 export const Skeleton = css`
-content: "";
-  display: inline-block;
-  background: linear-gradient(-90deg, #F0F0F0 0%, #F4F4F8 50%, #F0F0F0 100%);
-  background-size: 200px 32px;
-  border-radius: 4px;
-  animation: pulse 1.5s ease-in-out infinite;
-  @keyframes pulse {
-    0% {
-      background-position: 0% 0%;
-    }
-    100% {
-      background-position: -135% 0%;
-    }
-  }
+	content: "";
+	display: block;
+	background: linear-gradient(-90deg, #f0f0f0 0%, #f4f4f8 50%, #f0f0f0 100%);
+	background-size: 200px 32px;
+	border-radius: 4px;
+	animation: pulse 1.5s ease-in-out infinite;
+	@keyframes pulse {
+		0% {
+			background-position: 0% 0%;
+		}
+		100% {
+			background-position: -135% 0%;
+		}
+	}
 `;
-
-// const SkeletonLine = styled(Skeleton)`
-// width: 5.5em;
-// border-radius: 5px;
-
-// &::before {
-//   content: "\00a0";
-// }
-// `;
-
-// const SkeletonTable =
-// <div> 
-//   <SkeletonLine/>
-//   <SkeletonLine/>
-//   <SkeletonLine/>
-// <SkeletonLine/>
-// </div>;
-
-// export { Skeleton as default, SkeletonLine, SkeletonTable };
--- src/components/_helpers/SkeletonStatic.jsx
+++ src/components/_helpers/SkeletonStatic.jsx
@@ -5,7 +5,7 @@ import { css } from "styled-components";
 // Default content if component is empty - good for prototyping
 export const SkeletonStatic = css`
 content: "";
-  display: inline-block;
+  display: block;
   background: #F8F8F8;
   border: 5px solid white;
   background-size: 200px 32px;
