27ea2a0694ae6f57866cdbdfc80631d407d81419
Elizabeth Gallagher <EGallagher@digmap.com>
2019-08-05 17:44:36 -0700

    fixed space

--- src/components/elements/CardBars/LayerCardBar/LayerCardBar.jsx
+++ src/components/elements/CardBars/LayerCardBar/LayerCardBar.jsx
@@ -4,80 +4,80 @@ import PropTypes from "prop-types";
 import Icon from "atoms/Icon";
 import IconBlock from "blocks/IconBlock";
 import Bar from "blocks/Bar";
+import Grid from "helpers/Grid";
 import Card, { Piece } from "layout/Card";
 import Title from "base/Typography";
 
 import Switch from "atoms/Switch";
 
-const LayerSwitch = styled(Switch)`
-padding-right: 1em;
-`;
-
 const blockStyle = {
-	filter: "brightness(150%)",
-	lineHeight: "normal",
+  filter: "brightness(150%)",
+  lineHeight: "normal",
 };
 
 const disabledblockStyle = {
-	filter: "brightness(200%)",
-	lineHeight: "normal",
+  filter: "brightness(200%)",
+  lineHeight: "normal",
 };
 
 const Icons = (
-	<IconBlock style={blockStyle}>
-		<Icon icon={["far", "expand-arrows"]} />
-		<Icon icon={["far", "share"]} />
-		<Icon icon={["far", "filter"]} />
-	</IconBlock>
+  <IconBlock style={blockStyle}>
+    <Icon icon={["far", "expand-arrows"]} />
+    <Icon icon={["far", "share"]} />
+    <Icon icon={["far", "filter"]} />
+  </IconBlock>
 );
 
 const DisabledIcons = (
-	<IconBlock style={disabledblockStyle}>
-		<Icon icon={["far", "expand-arrows"]} />
-		<Icon icon={["far", "share"]} />
-		<Icon icon={["far", "filter"]} />
-	</IconBlock>
+  <IconBlock style={disabledblockStyle}>
+    <Icon icon={["far", "expand-arrows"]} />
+    <Icon icon={["far", "share"]} />
+    <Icon icon={["far", "filter"]} />
+  </IconBlock>
 );
 
-
 const SwitchContainer = styled.div`
-	display: flex;
-	vertical-align: baseline;
-	filter: ${props => (props.disabled ? "brightness(130%)" : "")};
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 1rem;
+  filter: ${props => (props.disabled ? "brightness(130%)" : "")};
+  &:last-child {
+    padding-left: 1rem;
+  }
 `;
 
 function LayerCardBar({ id, title, switchProps, disabled }) {
-	return (
-		<Piece id={id} title={title} disabled={disabled}>
-			{disabled ? (
-				<Bar
-					left={
-						<SwitchContainer disabled="true">
-							{" "}
-							<LayerSwitch /> 
-							<Title title={title} weight="normal" />
-						</SwitchContainer>
-					}
-					right={DisabledIcons}
-				/>
-			) : (
-				<Bar
-					left={
-						<SwitchContainer>
-							{" "}
-							<LayerSwitch {...switchProps} /> 
-							<Title title={title} weight="normal" />
-						</SwitchContainer>
-					}
-					right={Icons}
-				/>
-			)}
-		</Piece>
-	);
+  return (
+    <Piece id={id} title={title} disabled={disabled}>
+      {disabled ? (
+        <Bar
+          left={
+            <SwitchContainer disabled="true">
+              {" "}
+              <Switch />
+              <Title title={title} weight="normal" />
+            </SwitchContainer>
+          }
+          right={DisabledIcons}
+        />
+      ) : (
+        <Bar
+          left={
+            <SwitchContainer>
+              {" "}
+              <Switch {...switchProps} />
+              <Title title={title} weight="normal" />
+            </SwitchContainer>
+          }
+          right={Icons}
+        />
+      )}
+    </Piece>
+  );
 }
 LayerCardBar.propTypes = {
-	id: PropTypes.string,
-	title: PropTypes.any.isRequired,
-	disabled: PropTypes.bool,
+  id: PropTypes.string,
+  title: PropTypes.any.isRequired,
+  disabled: PropTypes.bool,
 };
 export default LayerCardBar;
