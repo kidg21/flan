581fc7f01f4213099efc807405ea52ca43f3df4d
Elizabeth Gallagher <EGallagher@digmap.com>
2019-08-07 16:10:16 -0700

    correct popcard

--- src/components/elements/PopupCard/PopCard.jsx
+++ src/components/elements/PopupCard/PopCard.jsx
@@ -2,27 +2,31 @@ import React from "react";
 import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 import Icon from "atoms/Icon";
-import Title, {Body} from "base/Typography";
 import Button from "atoms/Button";
 import Card, { Piece } from "layout/Card";
 import Bar from "blocks/Bar";
 
-function DialogueBox({ id, title, message, buttonLabel1, buttonLabel2 }) {
+function PopCard({ id, title, message, buttonLabel1, buttonLabel2 }) {
   return (
     <Card id={id}>
-      <Piece>
-        <Title title="Title"/>
-      </Piece>
-      <Piece>
-      <Title title="Supporting Text"/>
-      </Piece>
+      <Bar type="one" right={<Icon icon={["fal", "times"]} />} />
+      <Bar type="three" center={title} />
+      <Bar type="three" center={message} />
+      <Button buttonLabel={buttonLabel1} isRound={true} />
+      <Button
+        buttonLabel={buttonLabel2}
+        isSecondStandard={true}
+        isRound={true}
+      />
     </Card>
   );
 }
-DialogueBox.propTypes = {
+PopCard.propTypes = {
   id: PropTypes.string,
   title: PropTypes.string,
-  message: PropTypes.func,
+  message: PropTypes.string.isRequired,
+  buttonLabel1: PropTypes.string,
+  buttonLabel2: PropTypes.string,
 };
 
 export default PopCard;
