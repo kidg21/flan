8ba2a7c1e3db052cbe84048d7938ac735984d27a
Greg Person <gperson@digmap.com>
2019-08-08 12:26:42 -0700

    Icon Library - Moved the data arrays into a separate file.

---
+++ src/components/atoms/Icon/libraryIcon.data.js
@@ -0,0 +1,71 @@
+const placeholder = {
+  desc: "Short description of meaning and usage.",
+};
+
+// Navigation
+export const iconsNavigation = [
+    {
+      name: "menu",
+      icon: "menu",
+      desc: placeholder.desc,
+    },
+    {
+      name: "up",
+      icon: "up",
+      desc: placeholder.desc,
+    },
+    {
+      name: "down",
+      icon: "down",
+      desc: placeholder.desc,
+    },
+    {
+      name: "left",
+      icon: "left",
+      desc: placeholder.desc,
+    },
+    {
+      name: "right",
+      icon: "right",
+      desc: placeholder.desc,
+    },
+  ];
+
+// Brands
+export const iconsBrand = [
+  {
+    name: "apple",
+    icon: "{['fab', 'apple']}",
+    desc: placeholder.desc,
+  },
+  {
+    name: "facebook",
+    icon: "{['fab', 'facebook']}",
+    desc: placeholder.desc,
+  },
+  {
+    name: "firefox",
+    icon: "{['fab', 'firefox']}",
+    desc: placeholder.desc,
+  },
+  {
+    name: "ie",
+    icon: "{['fab', 'internet-explorer']}",
+    desc: placeholder.desc,
+  },
+  {
+    name: "twitter",
+    icon: "{['fab', 'twitter-square']}",
+    desc: placeholder.desc,
+  },
+  {
+    name: "windows",
+    icon: "{['fab', 'windows']}",
+    desc: placeholder.desc,
+  },
+];
+
+// for (let i = 0; i < iconsBrand.length; i++) {
+//   let brand = iconsBrand[i];
+//   brand.desc = brand.name + "apples";
+// }
--- src/components/work/Icon.stories.js
+++ src/components/work/Icon.stories.js
@@ -5,9 +5,10 @@ import Grid from "layout/Grid";
 import styled from "styled-components";
 import { colors, shadows, screen } from "Variables";
 import Title, { SubTitle, Description } from "base/Typography";
-import Icon from "atoms/Icon";
 import Bar from "blocks/Bar";
 import Card, { Piece } from "layout/Card";
+import Icon from "atoms/Icon";
+import { iconsBrand, iconsNavigation } from "atoms/Icon/libraryIcon.data.js";
 
 const IconGrid = styled(Grid)`
   grid-template-columns: repeat(1, 1fr);
@@ -20,19 +21,15 @@ const IconGrid = styled(Grid)`
   grid-auto-flow: dense;
 `;
 
-const placeholder = {
-  desc: "Short description of meaning and usage.",
-};
-
-function IconSet({ id, data }) {
+function IconSet({ data }) {
   return [
     data.map(item => (
-      <Piece id={id} key={item.id}>
+      <Piece key={item.name} id={item.name}>
         <Bar
           left={
             <Grid columns="1" gap="tiny">
               <Icon name={item.name} size="2x" />
-              <Title title={item.title} size="large" />
+              <Title title={item.name} size="large" />
               <Description title={item.desc} />
               <Title
                 title={"name=" + "'" + item.name + "'"}
@@ -52,51 +49,6 @@ function IconSet({ id, data }) {
   ];
 }
 
-const iconsBrand = [
-  {
-    id: "1",
-    title: "apple",
-    name: "apple",
-    icon: "{['fab', 'apple']}",
-    desc: placeholder.desc,
-  },
-  {
-    id: "2",
-    title: "facebook",
-    name: "facebook",
-    icon: "{['fab', 'facebook']}",
-    desc: placeholder.desc,
-  },
-  {
-    id: "3",
-    title: "firefox",
-    name: "firefox",
-    icon: "{['fab', 'firefox']}",
-    desc: placeholder.desc,
-  },
-  {
-    id: "4",
-    title: "ie",
-    name: "ie",
-    icon: "{['fab', 'internet-explorer']}",
-    desc: placeholder.desc,
-  },
-  {
-    id: "5",
-    title: "twitter",
-    name: "twitter",
-    icon: "{['fab', 'twitter-square']}",
-    desc: placeholder.desc,
-  },
-  {
-    id: "6",
-    title: "windows",
-    name: "windows",
-    icon: "{['fab', 'windows']}",
-    desc: placeholder.desc,
-  },
-];
-
 storiesOf("Work|Application/Libraries/Icon Library", module)
   .addDecorator(Padding)
   .add("Brand Icons", () => (
@@ -112,134 +64,10 @@ storiesOf("Work|Application/Libraries/Icon Library", module)
   .addDecorator(Padding)
   .add("Navigation Icons", () => (
     <Grid columns="1">
-      <Title title="Navigation Icons" size="large" style="underline" />
-      <IconGrid>
-        <Piece>
-          <Bar
-            left={
-              <Grid columns="1" gap="tiny">
-                <Icon name="menu" size="2x" />
-                <Title title="menu" size="large" />
-                <Description title="Short description of meaning and usage." />
-                <Title title="name='menu'" size="small" select />
-                <Title title="icon='bars'" size="small" select />
-              </Grid>
-            }
-          />
-        </Piece>
-        <Piece>
-          <Bar
-            left={
-              <Grid columns="1" gap="tiny">
-                <Icon name="up" size="2x" />
-                <Title title="up" size="large" />
-                <Description title="Short description of meaning and usage." />
-                <Title title="name='up'" size="small" select />
-                <Title title="icon={['far', 'angle-up']}" size="small" select />
-              </Grid>
-            }
-          />
-        </Piece>
-        <Piece>
-          <Bar
-            left={
-              <Grid columns="1" gap="tiny">
-                <Icon name="down" size="2x" />
-                <Title title="down" size="large" />
-                <Description title="Short description of meaning and usage." />
-                <Title title="name='down'" size="small" select />
-                <Title
-                  title="icon={['far', 'angle-down']}"
-                  size="small"
-                  select
-                />
-              </Grid>
-            }
-          />
-        </Piece>
-        <Piece>
-          <Bar
-            left={
-              <Grid columns="1" gap="tiny">
-                <Icon name="left" size="2x" />
-                <Title title="left" size="large" />
-                <Description title="Short description of meaning and usage." />
-                <Title title="name='left'" size="small" select />
-                <Title
-                  title="icon={['far', 'angle-left']}"
-                  size="small"
-                  select
-                />
-              </Grid>
-            }
-          />
-        </Piece>
-        <Piece>
-          <Bar
-            left={
-              <Grid columns="1" gap="tiny">
-                <Icon name="right" size="2x" />
-                <Title title="right" size="large" />
-                <Description title="Short description of meaning and usage." />
-                <Title title="name='right'" size="small" select />
-                <Title
-                  title="icon={['far', 'angle-right']}"
-                  size="small"
-                  select
-                />
-              </Grid>
-            }
-          />
-        </Piece>
-      </IconGrid>
-    </Grid>
-  ));
-
-const iconsMap = [
-  {
-    id: "1",
-    title: "menu",
-    icon: "menu",
-    name: "menu",
-    desc: placeholder.desc,
-  },
-  {
-    id: "2",
-    title: "up",
-    icon: "up",
-    name: "up",
-    desc: placeholder.desc,
-  },
-  {
-    id: "3",
-    title: "down",
-    icon: "down",
-    name: "down",
-    desc: placeholder.desc,
-  },
-  {
-    id: "4",
-    title: "left",
-    icon: "left",
-    name: "left",
-    desc: placeholder.desc,
-  },
-  {
-    id: "5",
-    title: "right",
-    icon: "right",
-    name: "right",
-    desc: placeholder.desc,
-  },
-];
-
-storiesOf("Work|Application/Libraries/Icon Library", module)
-  .addDecorator(Padding)
-  .add("Map Icons", () => (
-    <Grid columns="1">
       <Title title="Map Icons" size="large" style="underline" />
       <IconGrid>
-        <IconSet data={iconsMap} />
+        <IconSet data={iconsNavigation} />
       </IconGrid>
     </Grid>
   ));
+
