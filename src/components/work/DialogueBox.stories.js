aab58f719c26c3b93ae2edf2a1e393a774ae1371
Elizabeth Gallagher <EGallagher@digmap.com>
2019-08-12 11:44:06 -0700

    created inline scroll container for dialogue boxes

--- src/components/work/DialogueBox.stories.js
+++ src/components/work/DialogueBox.stories.js
@@ -7,12 +7,15 @@ import DialogueBox from "elements/DialogueBox";
 import Title, { Body } from "base/Typography";
 import Bar from "blocks/Bar";
 import SelectMenu from "atoms/SelectMenu";
+import Card, { Piece } from "layout/Card";
 import Grid from "layout/Grid";
+import List, { ListItem } from "blocks/List";
 import Button from "atoms/Button";
 import TextInput from "atoms/TextInput";
 import Icon from "atoms/Icon";
 import Form, { Section, SectionName } from "layout/Form";
 import DMPlogo from "images/DMPlogo.jpg";
+import Container from "atoms/Container";
 
 const image = {
   src: DMPlogo,
@@ -135,6 +138,202 @@ storiesOf("Work|Dialogue Box", module)
           right={
             <Grid columns="2">
               <Button buttonLabel="Done" type="solid" />
+              <Button buttonLabel="Cancel" />
+            </Grid>
+          }
+        />
+      }
+    />
+  ))
+  .add("Adding a New User Group", () => (
+    <DialogueBox
+      header={<Bar left={<Title title="Create New User Group" />} />}
+      content={
+        <Bar
+          left={
+            <Section>
+              <TextInput
+                inputLabel="Group Name"
+                placeholder="Type a descriptive name for your new group."
+              />
+              <SelectMenu
+                multiSelect="true"
+                inputLabel="+ Add Group Users"
+                options={[
+                  { value: "1", label: "Adam" },
+                  { value: "2", label: "Oliver" },
+                  { value: "3", label: "Matt" },
+                  { value: "4", label: "Joe" },
+                ]}
+              />
+              <Piece>
+                <Container setHeight="12rem">
+                  <List>
+                    <ListItem
+                      label="User 3"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 2"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 1"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 3"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 3"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 2"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 1"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 3"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 3"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 2"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 1"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                    <ListItem
+                      label="User 3"
+                      action={<Icon icon={["far", "cog"]} />}
+                    />
+                  </List>
+                </Container>
+              </Piece>
+            </Section>
+          }
+        />
+      }
+      footer={
+        <Bar
+          right={
+            <Grid columns="2">
+              <Button buttonLabel="Create" type="solid" />
+              <Button buttonLabel="Cancel" />
+            </Grid>
+          }
+        />
+      }
+    />
+  ))
+  .add("Editing a Role", () => (
+    <DialogueBox
+      header={<Bar left={<Title title="Edit existing User Role" />} />}
+      content={
+        <Bar
+          left={
+            <Section>
+              <SelectMenu
+                placeholder="Select one or many roles for this user."
+                inputLabel="Assign Roles"
+                options={[
+                  { value: "Marketing", label: "Marketing" },
+                  { value: "Acquisitions", label: "Acquisitions" },
+                  { value: "Developer", label: "Builder / Developer" },
+                  { value: "Admin", label: "Admin" },
+                ]}
+              />
+              <TextInput
+                inputLabel="Group Name"
+                placeholder="Type a descriptive name for your new group."
+              />
+              <SelectMenu
+                multiSelect="true"
+                inputLabel="+ Add Group Users"
+                options={[
+                  { value: "1", label: "Adam" },
+                  { value: "2", label: "Oliver" },
+                  { value: "3", label: "Matt" },
+                  { value: "4", label: "Joe" },
+                ]}
+              />
+              <Container>
+                <List>
+                  <ListItem
+                    label="User 3"
+                    action={<Icon icon={["far", "cog"]} />}
+                  />
+                  <ListItem
+                    label="User 2"
+                    action={<Icon icon={["far", "cog"]} />}
+                  />
+                  <ListItem
+                    label="User 1"
+                    action={<Icon icon={["far", "cog"]} />}
+                  />
+                </List>
+              </Container>
+            </Section>
+          }
+        />
+      }
+      footer={
+        <Bar
+          right={
+            <Grid columns="2">
+              <Button buttonLabel="Save" type="solid" />
+              <Button buttonLabel="Cancel" />
+            </Grid>
+          }
+        />
+      }
+    />
+  ))
+  .add("Add a new User", () => (
+    <DialogueBox
+      header={<Bar left={<Title title="+ Add a New User" />} />}
+      content={
+        <Bar
+          left={
+            <Section>
+              <TextInput
+                inputLabel="User Name"
+                placeholder="Type the name for your new user."
+              />
+              <TextInput
+                inputLabel="User Account"
+                placeholder="Type in User email or account name "
+              />
+              <SelectMenu
+                multiSelect="true"
+                placeholder="Select one or many roles for this user."
+                inputLabel="Assign Roles"
+                options={[
+                  { value: "Marketing", label: "Marketing" },
+                  { value: "Acquisitions", label: "Acquisitions" },
+                  { value: "Developer", label: "Builder / Developer" },
+                  { value: "Admin", label: "Admin" },
+                ]}
+              />
+            </Section>
+          }
+        />
+      }
+      footer={
+        <Bar
+          right={
+            <Grid columns="2">
+              <Button buttonLabel="Create" type="solid" />
               <Button buttonLabel="Cancel" />
             </Grid>
           }
