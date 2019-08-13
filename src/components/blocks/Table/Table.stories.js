4efcb9db98f0f9212c2f934a3a3714ae6f76a28a
DIGMAP\ESugimoto <ESugimoto@digmap.com>
2019-08-13 14:05:57 -0700

    update story to use cellcache

--- src/components/blocks/Table/Table.stories.js
+++ src/components/blocks/Table/Table.stories.js
@@ -1,32 +1,50 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Table from "blocks/Table";
-import DataTable from "./DataTable";
 import { withInfo } from "@storybook/addon-info";
 import { Padding } from "helpers/Display";
 import { MultiGridWrapper, CellWrapper } from "./TableView";
-import { MultiGrid, AutoSizer } from "react-virtualized";
+import { MultiGrid, AutoSizer, CellMeasurer, CellMeasurerCache } from "react-virtualized";
 const data = [
-  { id: "a", color: "Owners/Units", name: "Multiple Owners (2 Units)" },
-  { id: "b", color: "Master Parcel No.", name: "387483675638" },
-  { id: "c", color: "Zoning", name: "No Zone" }
+  { id: "a", name: "Owners/Units", value: "Multiple Owners (2 Units)" },
+  { id: "b", name: "Master Parcel No.", value: "387483675638" },
+  { id: "c", name: "Zoning", value: "No Zone" },
 ];
-function cellRenderer({ columnIndex, key, parent, style, rowIndex}) {
-  let header = false;
-  if (rowIndex === 0) header = true;
-  return (
-    <CellWrapper style={style} key={key} isHeader={header} evenRow={rowIndex % 2 === 0}>
-      {"sampleText"}
-    </CellWrapper>
-  )
-}
+
 storiesOf("Blocks|Table", module)
   .addDecorator(Padding)
   .addDecorator(withInfo)
-  .add("Simple", () => <Table data={data} />)
-  .add("Heavy Data", () => <DataTable />)
+  .add("Simple", () => (
+    <div>
+      <Table data={data} />
+      <Table data={data} width="half" />
+      <Table data={data} width="full" />
+    </div>
+  ))
   .add("React Virtualized Table", () =>
     React.createElement(() => {
+      const cache = new CellMeasurerCache({
+        defaultWidth: 100,
+        minWidth: 50,
+        fixedHeight: true,
+      });
+      function cellRenderer({ columnIndex, key, parent, style, rowIndex}) {
+        let header = false;
+        if (rowIndex === 0) header = true;
+        return (
+          <CellMeasurer
+            cache={cache}
+            columnIndex={columnIndex}
+            key={key}
+            parent={parent}
+            rowIndex={rowIndex}
+          >
+            <CellWrapper style={style} isHeader={header} evenRow={rowIndex % 2 === 0}>
+              {"sampleText"}
+            </CellWrapper>
+          </CellMeasurer>
+        )
+      }
       return (
         <MultiGridWrapper>
           <AutoSizer disableHeight>
@@ -34,7 +52,7 @@ storiesOf("Blocks|Table", module)
               <MultiGrid
                 cellRenderer={cellRenderer}
                 columnCount={10}
-                columnWidth={100}
+                columnWidth={cache.columnWidth}
                 height={200}
                 rowCount={20}
                 rowHeight={25}
@@ -46,4 +64,4 @@ storiesOf("Blocks|Table", module)
         </MultiGridWrapper>
       )
     })
-  )
+  );
