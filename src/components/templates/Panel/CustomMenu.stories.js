e383f35d3ea1b12d8d06a81b678d0e03325dfe08
Elizabeth Gallagher <EGallagher@digmap.com>
2019-08-20 17:05:09 -0700

    Used Interactive List for Command panel

--- src/components/templates/Panel/CustomMenu.stories.js
+++ src/components/templates/Panel/CustomMenu.stories.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import MainPanelHeader from "elements/PanelHeaders/MainPanelHeader";
-import NavigationCardBar from "elements/CardBars/NavigationCardBar";
 import Panel, { PanelSection } from "layout/Panel";
-import Command from "atoms/Command";
-import Bar from "blocks/Bar";
+import List, { ListItem } from "blocks/List";
 
 storiesOf("Templates|Panel", module).add("Custom Commands", () =>
   React.createElement(() => {
     return (
       <Panel>
         <PanelSection>
-          <MainPanelHeader title="Custom Commands" />
+          <MainPanelHeader title="Custom Actions" />
         </PanelSection>
         <PanelSection body>
-          <Bar left={<Command label="Custom Command 1" />} />
-          <Bar left={<Command label="Custom Command 1" />} />
-          <Bar left={<Command label="Custom Command 1" />} />
-          <Bar left={<Command label="Custom Command 1" />} />
-          <Bar left={<Command label="Custom Command 1" />} />
-          <Bar left={<Command label="Custom Command 1" />} />
-          <Bar left={<Command label="Custom Command 1" />} />
-          <Bar left={<Command label="Custom Command 1" />} />
+          <List interactive>
+            <ListItem label="Custom Command 1" onClick="" />
+            <ListItem label="Custom Command 2" onClick="" />
+            <ListItem label="Custom Command 3" onClick="" />
+            <ListItem label="Custom Command 4" onClick="" />
+            <ListItem label="Custom Command 5" onClick="" />
+            <ListItem label="Custom Command 6" onClick="" />
+            <ListItem label="Custom Command 7" onClick="" />
+            <ListItem label="Custom Command 8" onClick="" />
+          </List>
         </PanelSection>
       </Panel>
     );
