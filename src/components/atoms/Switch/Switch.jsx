0eb116485a0eb180b39882e91281683af04ffc38
Greg Person <gperson@digmap.com>
2019-08-27 17:06:48 -0700

    Switch - Adding standard input states, props, etc.

--- src/components/atoms/Switch/Switch.jsx
+++ src/components/atoms/Switch/Switch.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { colors } from "Variables";
+import { DisabledContext } from "States";
+import { InputLabel, HelpText, ErrorText } from "layout/Form";
 
 const SwitchContainer = styled.div`
+  color: ${(props) => {
+    return props.checkboxColor || "";
+  }};
+  background-color: ${(props) => {
+    return props.fillColor || colors.white;
+  }};
+  border-color: ${(props) => {
+    return props.borderColor || colors.grey_40;
+  }};
   position: relative;
   display: inline-block;
   vertical-align: middle;
 `;
+
 const HiddenSwitch = styled.input.attrs({ type: "checkbox" })`
   border: 0;
   clip: rect(0 0 0 0);
-  /* clipath: inset(50%); */
   height: 1px;
   margin: -1px;
   overflow: hidden;
@@ -28,76 +39,151 @@ const HiddenSwitch = styled.input.attrs({ type: "checkbox" })`
 const Circle = styled.div`
   position: absolute;
   z-index: 1;
-  background: ${props => {
-    return props.checked ? "white" : "white";
+  background: ${(props) => {
+    return props.checked ? colors.success_tint : colors.grey_light;
   }};
-  border: ${props => {
-    return props.checked ? "1px solid #75ab3f" : "1px solid darkgray";
+  border: 1px solid;
+  border-color: ${(props) => {
+    return props.checked ? colors.success : colors.grey_40;
   }};
   border-radius: 40px;
   width: 15px;
   line-height: normal;
   height: 14px;
   transition: transform 300ms ease-in-out;
-  transform: ${props => {
-    return props.checked ? "translateX(15px)" : "translateX(-1px)";
+  transform: ${(props) => {
+    return props.checked ? "translateX(12px)" : "translateX(0)";
   }};
+  &[disabled],
+  &[readonly] {
+    border-color: ${(props) => {
+    return props.disabled ? colors.grey_40 : "";
+  }};
+  }
 `;
 
 const StyledSwitch = styled.div`
   width: 30px;
   display: block;
   height: 16px;
+  border: 1px solid;
+  border-color: ${(props) => {
+    return props.checked ? colors.success : colors.grey_40;
+  }};
   border-radius: 23px;
-  border: ${props => {
-    return props.checked ? "1px solid #94d850" : "1px solid darkgray";
+  transition: 3000ms ease-in-out;
+  background-image: ${(props) => {
+    return props.checked
+      ? `linear-gradient(${colors.success_tint}, ${colors.success_light})`
+      : colors.white;
   }};
-  background-image: ${props => {
-    return props.checked ? "linear-gradient(#75ab3f, #94d850);" : "white";
-  }};
-  transition: 0.4s ease;
   cursor: pointer;
+  &[disabled],
+  &[readonly] {
+    background-image: ${(props) => {
+    return props.disabled ? "none" : "";
+  }};
+    background: ${(props) => {
+    return props.disabled ? colors.grey_20 : "";
+  }};
+  }
 `;
 
-const Toggle = ({ checked, ...props }) => {
-  const [isChecked, setChecked] = useState(checked);
+const ToggleWrapper = styled.label`
+  grid-area: one;
+  /* background-color: ${colors.grey_light}; */
+  &[disabled],
+  &[readonly] {
+    cursor: not-allowed;
+    pointer-events: none;
+    /* user-select: none; */
+  }
+`;
 
+function Toggle({ checked, disabled, ...props }) {
+  let checkboxColor;
+  let fillColor;
+  let borderColor;
+  const isDisabled = typeof disabled === "boolean" ? disabled : useContext(DisabledContext);
+  if (isDisabled) {
+    checkboxColor = colors.grey_40;
+    fillColor = colors.grey_20;
+    borderColor = checkboxColor;
+  }
+  const [isChecked, setChecked] = useState(checked);
   const { onChange, ...remainingProps } = props;
   function onClick(e) {
-    setChecked(state => {
+    setChecked((state) => {
       return !state;
     });
     e.preventDefault();
     if (onChange) onChange(e);
   }
-
   return (
-    <SwitchContainer>
+    <SwitchContainer disabled={disabled}>
       <HiddenSwitch checked={isChecked} onClick={onClick} {...remainingProps} />
-      <StyledSwitch checked={isChecked}>
-        <Circle checked={isChecked} {...remainingProps} />
+      <StyledSwitch
+        fillColor={fillColor}
+        borderColor={borderColor}
+        checked={isChecked}
+        disabled={disabled}
+      >
+        <Circle checked={isChecked} disabled={disabled} {...remainingProps} />
       </StyledSwitch>
     </SwitchContainer>
   );
-};
+}
+
+const ToggleLabel = styled(InputLabel)`
+  grid-area: two;
+`;
+
+const ToggleContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-areas: 'one two';
+  grid-gap: 0.75rem;
+  align-content: flex-start;
+  /* color: ${(props) => {
+    return props.inputTextColor || "";
+  }}; */
+`;
+
+function Switch({
+  checked, disabled, id, isRequired, inputLabel, ...props
+}) {
+  return (
+    <ToggleContainer inputLabel={inputLabel} isRequired={isRequired}>
+      <ToggleWrapper id={id} disabled={disabled}>
+        <Toggle id={id} checked={checked} disabled={disabled} {...props} />
+      </ToggleWrapper>
+      {inputLabel ? <ToggleLabel htmlFor={id} inputLabel={inputLabel} disabled={disabled} /> : null}
+    </ToggleContainer>
+  );
+}
 
 Toggle.propTypes = {
   id: PropTypes.string,
   checked: PropTypes.bool,
+  disabled: PropTypes.bool,
   onChange: PropTypes.func,
 };
-
-const Switch = ({ checked, id, ...props }) => {
-  return (
-    <label>
-      <Toggle checked={checked} id={id} {...props} />
-    </label>
-  );
+Toggle.defaultProps = {
+  id: null,
+  checked: false,
+  disabled: false,
+  onChange: null,
 };
 
 Switch.propTypes = {
   id: PropTypes.string,
   checked: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
+Switch.defaultProps = {
+  id: null,
+  checked: false,
+  disabled: false,
 };
 
 export default Switch;
