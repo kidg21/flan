09017c9efbf3875154ad510272c1c2598a7b2d05
Greg Person <gperson@digmap.com>
2019-09-11 18:31:17 -0700

    Data Range - Updated to use standard structure and props - Configured propTypes - Added documentation, stories, knobs

--- src/components/blocks/DataRange/DataRange.stories.js
+++ src/components/blocks/DataRange/DataRange.stories.js
@@ -1,9 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from "react";
-import styled, { css } from "styled-components";
 import { storiesOf } from "@storybook/react";
 import { Padding } from "helpers/Display";
-import DataRange from "./DataRange";
 import { withInfo } from "@storybook/addon-info";
+import { withKnobs, text, boolean, object } from "@storybook/addon-knobs";
+import Form, { Section } from "layout/Form";
+import DataRange from "blocks/DataRange";
+import DataRangeNotes from "./DataRange.md";
 
 const options = [
   { value: "0", label: "0" },
@@ -11,19 +14,114 @@ const options = [
   { value: "2", label: "2" },
   { value: "3", label: "3" },
   { value: "4", label: "4" },
-  { value: "5", label: "5" }
+  { value: "5", label: "5" },
 ];
 
 storiesOf("Blocks|Data Range", module)
-  .addDecorator(withInfo)
+  .addParameters({
+    info: {
+      text: "Range Slider info goes here...",
+    },
+    notes: {
+      markdown: DataRangeNotes,
+    },
+  })
   .addDecorator(Padding)
-  .add("Default", () => (
-    <DataRange
-      label="Default Data Range"
-      labelMin="Min"
-      optionsMin={options}
-      labelMax="Max"
-      optionsMax={options}
-    />
-  ))
-  .add("Skeleton", () => <DataRange />);
+  .addDecorator(withKnobs)
+  .add(
+    "Documentation",
+    withInfo()(() => {
+      return (
+        <Form columns="1">
+          <DataRange
+            label="Data Range (default)"
+            labelMin="Min"
+            labelMax="Max"
+            helpText="This help text has been passed through a prop!"
+          />
+          <DataRange
+            label="Data Range (with options)"
+            labelMin="Min"
+            optionsMin={options}
+            labelMax="Max"
+            optionsMax={options}
+            helpText="This help text has been passed through a prop!"
+          />
+        </Form>
+      );
+    }),
+  )
+  .add("Knobs", () => {
+    return (
+      <DataRange
+        label={text("label", "Data Label", "Data Range")}
+        labelMin={text("min label", "Min", "Data Range")}
+        optionsMin={object("options (min)", [{ value: "0", label: "0" }], "Data Range")}
+        labelMax={text("max label", "Max", "Data Range")}
+        optionsMax={object("options (max)", [{ value: "0", label: "0" }], "Data Range")}
+        isRequired={boolean("required", false, "Data Range")}
+        helpText={text("help text", "Help text goes here...", "Data Range")}
+        errorText={text("error text", "", "Data Range")}
+        disabled={boolean("disabled", false, "Data Range")}
+      />
+    );
+  })
+  .add("States", () => {
+    return (
+      <Form title="States" columns="1">
+        <Section title="Input Range (default)">
+          <DataRange
+            label="Input Range"
+            labelMin="Min"
+            labelMax="Max"
+            helpText="This help text has been passed through a prop!"
+          />
+          <DataRange
+            label="Input Range (error)"
+            labelMin="Min"
+            labelMax="Max"
+            helpText="This help text has been passed through a prop!"
+            errorText="Out of range!"
+          />
+          <DataRange
+            label="Input Range (disabled)"
+            labelMin="Min"
+            labelMax="Max"
+            helpText="This help text has been passed through a prop!"
+            disabled
+          />
+        </Section>
+        <Section title="Select Range">
+          <DataRange
+            label="Select Range"
+            labelMin="Min"
+            optionsMin={options}
+            labelMax="Max"
+            optionsMax={options}
+            helpText="This help text has been passed through a prop!"
+          />
+          <DataRange
+            label="Select Range (error)"
+            labelMin="Min"
+            optionsMin={options}
+            labelMax="Max"
+            optionsMax={options}
+            helpText="This help text has been passed through a prop!"
+            errorText="Out of range!"
+          />
+          <DataRange
+            label="Select Range (disabled)"
+            labelMin="Min"
+            optionsMin={options}
+            labelMax="Max"
+            optionsMax={options}
+            helpText="This help text has been passed through a prop!"
+            disabled
+          />
+        </Section>
+      </Form>
+    );
+  })
+  .add("Skeleton", () => {
+    return <DataRange />;
+  });
