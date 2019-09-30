d9af43081c7bc2fb25880aa9a94017f6b8ac2169
Elizabeth Gallagher <EGallagher@digmap.com>
2019-09-30 12:47:12 -0700

    fixed text input

--- src/components/atoms/TextInput/TextInput.jsx
+++ src/components/atoms/TextInput/TextInput.jsx
@@ -7,7 +7,7 @@ import Grid from "layout/Grid";
 
 const TextInputContainer = styled(Grid)`
   color: ${(props) => {
-    return props.inputTextColor || "";
+    return props.theme.text[props.inputTextColor] || "";
   }};
   width: 100%;
 `;
