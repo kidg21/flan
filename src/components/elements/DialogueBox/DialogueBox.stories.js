4a8f408992efd79b4ac8615c01e56599957d6433
Elizabeth Gallagher <EGallagher@digmap.com>
2019-10-02 11:08:22 -0700

    removed a conflict that was preventing storybook from running

--- src/components/elements/DialogueBox/DialogueBox.stories.js
+++ src/components/elements/DialogueBox/DialogueBox.stories.js
@@ -93,7 +93,7 @@ storiesOf("Elements|Dialogue Box", module)
               { value: "4", label: "lightbox" },
             ]}
           />
-          <Command label="Add new Location" icon="plus"/>
+          {/* <Command label="Add new Location" icon="plus"/> */}
           <SelectMenu
             label="File Type"
             options={[
