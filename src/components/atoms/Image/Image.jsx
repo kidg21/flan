f2ae77e5039ce73f8d6da7c1cb9c9f2a5d18a597
Greg Person <gperson@digmap.com>
2019-10-11 17:59:05 -0700

    Image (new) - Created an Image atom that wraps the HTML <img> tag - Set up stories - Added images to '\src\images' for development

---
+++ src/components/atoms/Image/Image.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable linebreak-style */
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import Title from "base/Typography";
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const ImageLabel = styled(Title)`
+  /* color: red; */
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background: white;
+  margin-bottom: 1em;
+  padding: 1em;
+  text-align: center;
+`;
+
+const ImageWrapper = styled.img`
+  border-radius: ${(props) => {
+    return props.borderRadius || "";
+  }};
+  width: ${(props) => {
+    return props.width || "inherit";
+  }};
+  height: ${(props) => {
+    return props.height || "inherit";
+  }};
+  &:after {
+    content: "";
+  }
+`;
+
+function Image({
+  src, width, height, type, label,
+}) {
+  let borderRadius;
+  switch (type) {
+    case "circle":
+      borderRadius = "100%";
+      break;
+    case "thumb":
+      width = "75px";
+      height = "75px";
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <ImageWrapper
+      src={src}
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      label={label}
+    />
+  );
+}
+
+Image.propTypes = {
+  src: PropTypes.string,
+};
+Image.defaultProps = {
+  src: null,
+};
+
+export default Image;
---
+++ src/components/atoms/Image/Image.stories.js
@@ -0,0 +1,38 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-filename-extension */
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Padding } from "helpers/Display";
+import Grid from "layout/Grid";
+import Image from "atoms/Image";
+
+storiesOf("Atoms|Image", module)
+  .addDecorator(Padding)
+  .add("Default", () => {
+    return (
+      <Grid>
+        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKFattKrNRvWlq7W5k_19wjiYpmqVUFgw4vDIrgWL5l2BQuRAE" />
+        <Image
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKFattKrNRvWlq7W5k_19wjiYpmqVUFgw4vDIrgWL5l2BQuRAE"
+          type="thumb"
+        />
+        <Image
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKFattKrNRvWlq7W5k_19wjiYpmqVUFgw4vDIrgWL5l2BQuRAE"
+          label="This is a label"
+        />
+        <Image
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKFattKrNRvWlq7W5k_19wjiYpmqVUFgw4vDIrgWL5l2BQuRAE"
+          width="50px"
+          height="50px"
+          label="This is a label"
+        />
+        <Image
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKFattKrNRvWlq7W5k_19wjiYpmqVUFgw4vDIrgWL5l2BQuRAE"
+          width="50px"
+          height="50px"
+          type="circle"
+          label="This is a label"
+        />
+      </Grid>
+    );
+  });
