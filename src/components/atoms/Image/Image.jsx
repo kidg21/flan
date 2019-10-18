2326419b154e50f2615df6b37c388f94601efecb
Greg Person <gperson@digmap.com>
2019-10-17 17:59:34 -0700

    Image - Simplfied - Refactored 'circle' and 'border' as boolean props - Removed 'type' switch - Configured stores, knobs - Added documentation

--- src/components/atoms/Image/Image.jsx
+++ src/components/atoms/Image/Image.jsx
@@ -2,81 +2,40 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import Title from "base/Typography";
-
-const ImageContainer = styled.div`
-  position: relative;
-  width: min-content;
-  /* height: auto; */
-  height: 100%;
-`;
-
-const ImageLabel = styled(Title)`
-  /* color: red; */
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  background: white;
-  /* margin-bottom: 1em; */
-  padding: .25em;
-  text-align: center;
-  max-height: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
 
 const ImageWrapper = styled.img`
-  border-radius: ${(props) => {
-    return props.borderRadius || "";
-  }};
   width: ${(props) => {
     return props.width || "inherit";
   }};
-  height: ${(props) => {
-    return props.height || "inherit";
+  height: auto;
+  border-radius: ${(props) => {
+    return props.circle ? "100%" : "5px";
   }};
-  &:after {
-    content: "";
-  }
+  box-shadow: ${(props) => {
+    return props.border ? `0 0 0 2px ${props.theme.palette.border}` : "";
+  }};
 `;
 
 function Image({
-  src, width, height, type, label,
+  alt, border, circle, src, width,
 }) {
-  let borderRadius;
-  switch (type) {
-    case "circle":
-      borderRadius = "100%";
-      break;
-    case "thumb":
-      width = "75px";
-      height = "75px";
-      break;
-    default:
-      break;
-  }
-
   return (
-    <ImageContainer>
-      <ImageWrapper
-        src={src}
-        width={width}
-        height={height}
-        borderRadius={borderRadius}
-        // label={label}
-      />
-      <ImageLabel>{label}</ImageLabel>
-    </ImageContainer>
+    <ImageWrapper alt={alt} border={border} circle={circle} src={src} title={alt} width={width} />
   );
 }
 
 Image.propTypes = {
+  alt: PropTypes.string.isRequired,
+  border: PropTypes.boolean,
+  circle: PropTypes.boolean,
   src: PropTypes.string,
+  width: PropTypes.number,
 };
 Image.defaultProps = {
   src: null,
+  border: false,
+  circle: false,
+  width: null,
 };
 
 export default Image;
