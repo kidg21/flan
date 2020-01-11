e37e75c733a94b3f76069eb29b2f45efafb15e35
Greg Person <gperson@digmap.com>
2020-01-10 17:57:33 -0800

    Panel - Trying to cut excess code

--- src/components/layout/Panel/Panel.jsx
+++ src/components/layout/Panel/Panel.jsx
@@ -4,6 +4,18 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { PlaceholderText } from "helpers/Placeholders.jsx";
 
+// Temporary: These variables will be removed once Card updates are merged
+const colors = {
+  shade5: "hsla(34, 5%, 12%, 0.25)",
+  shade6: "hsla(34, 5%, 12%, 0.3)",
+};
+const shadows = {
+  outerShadow: `
+    ${colors.shade6} 0 0 1px,
+    ${colors.shade5} 0 0 1rem
+    `,
+};
+
 const PanelWrapper = styled.div`
   position: absolute;
   background: ${(props) => {
@@ -16,14 +28,8 @@ const PanelWrapper = styled.div`
   align-content: stretch;
   width: 100%;
   height: 100%;
-  border: .5px solid ${(props) => {
-    return props.theme.palette.grey5;
-  }};
   max-height: 100vh;
   overflow: hidden;
-  transform: none;
-  transition: all 0.3s ease-in-out;
-  /* box-sizing: content-box; */
   -webkit-overflow-scrolling: touch;
   /* Prototype Content - displays when a Panel Section is empty */
   &:empty {
@@ -41,43 +47,16 @@ const SectionWrapper = styled.section`
   position: relative;
   display: flex;
   flex-direction: column;
-  /* flex: auto; */
-  flex: ${(props) => {
-    return props.sectionFlex || "auto";
-  }};
-  /* height: ${(props) => {
-    return props.body ? "inherit" : "";
-  }}; */
-  /* padding: 1rem; */
-  padding: ${(props) => {
-    return props.header ? "" : "1rem";
-  }};
+  flex: auto;
+  padding: 1rem;
   z-index: 0;
-  /* z-index: ${(props) => {
-    return props.body ? "0" : "1";
-  }}; */
   overflow-y: scroll;
-  /* overflow-x: ${(props) => {
-    return props.header ? "visible" : "hidden";
-  }}; */
-  /* overflow-y: ${(props) => {
-    if (props.body) {
-      return "scroll";
-    } else if (props.header) {
-      return "visible";
-    }
-    return "";
-  }}; */
   max-height: 100vh;
-  /* box-shadow: ${(props) => {
-    return props.body ? "none" : props.theme.shadows.shadowV;
-  }}; */
-  transition: all 0.2s ease-in-out;
   /* Prototype Content - displays when a Panel Section is empty */
   &:empty {
     &:before {
       ${PlaceholderText};
-      content: "PanelSection";
+      content: "PanelBody";
       color: ${(props) => {
     return props.theme.text.primary;
   }};
@@ -85,47 +64,44 @@ const SectionWrapper = styled.section`
   }
 `;
 
-function PanelSection({
-  children, className, header, footer, id,
+function PanelBody({
+  children, className,
 }) {
-  let sectionFlex;
-  if (header) {
-    sectionFlex = "none";
-    // sectionJustify = "flex-start";
-  }
-  if (footer) {
-    sectionFlex = "none";
-    // sectionJustify = "flex-end";
-  }
   return (
     <SectionWrapper
-      sectionFlex={sectionFlex}
       className={className}
-      footer={footer}
-      header={header}
-      id={id}
     >
       {children}
     </SectionWrapper>
   );
 }
 
+PanelBody.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+PanelBody.defaultProps = {
+  children: null,
+  className: null,
+};
+
+const PanelSection = styled(PanelBody)`
+  flex: none;
+  padding: 0;
+  overflow: hidden;
+  z-index: 1;
+  box-shadow: ${shadows.outerShadow};
+`;
 function Panel({
   id, children, footer, header,
 }) {
-  let sectionFlex;
-  if (header) {
-    sectionFlex = "none";
-  }
   return (
     <PanelWrapper
       id={id}
     >
-      {header ? <PanelSection header>{header}</PanelSection> : null}
-      <PanelSection>
-        {children}
-      </PanelSection>
-      {footer ? <PanelSection header>{footer}</PanelSection> : null}
+      {header ? <PanelSection>{header}</PanelSection> : null}
+      <PanelBody>{children}</PanelBody>
+      {footer ? <PanelSection>{footer}</PanelSection> : null}
     </PanelWrapper>
   );
 }
--- src/components/layout/Panel/Panel.stories.js
+++ src/components/layout/Panel/Panel.stories.js
@@ -1,66 +1,19 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
-import Card, { CardList } from "../Card/Card.jsx";
-import Panel from "../Panel/Panel.jsx";
+import Title from "base/Typography";
+import Card, { CardList } from "layout/Card";
+import Panel from "layout/Panel";
 
 storiesOf("Layout|Panel", module)
   .addDecorator(checkA11y)
-  // .add("Panel - Outer Wrapper", () => {
-  //   return <Panel />;
-  // })
-  // .add("Panel - 1 Section (default)", () => {
-  //   return (
-  //     <Panel>
-  //       <PanelSection body />
-  //     </Panel>
-  //   );
-  // })
-  // .add("Panel - 2 Sections", () => {
-  //   return (
-  //     <Panel>
-  //       <PanelSection />
-  //       <PanelSection body />
-  //     </Panel>
-  //   );
-  // })
-  // .add("Panel - 3 Sections", () => {
-  //   return (
-  //     <Panel>
-  //       <PanelSection />
-  //       <PanelSection body />
-  //       <PanelSection />
-  //     </Panel>
-  //   );
-  // })
-  // .add("Panel - 4 Sections (2 body)", () => {
-  //   return (
-  //     <Panel>
-  //       <PanelSection />
-  //       <PanelSection body />
-  //       <PanelSection body />
-  //       <PanelSection />
-  //     </Panel>
-  //   );
-  // })
-  // .add("Panel - 5 Sections (2 body)", () => {
-  //   return (
-  //     <Panel>
-  //       <PanelSection />
-  //       <PanelSection body />
-  //       <PanelSection />
-  //       <PanelSection body />
-  //       <PanelSection />
-  //     </Panel>
-  //   );
-  // })
   .add("Panel - with Card List", () => {
     return (
       <Panel
+        id="Panel"
         header={<Card />}
+        footer={<Card />}
       >
-        {/* <PanelSection /> */}
-        {/* <PanelSection> */}
         <CardList>
           <Card />
           <Card />
@@ -70,8 +23,54 @@ storiesOf("Layout|Panel", module)
           <Card />
           <Card />
         </CardList>
-        {/* </PanelSection> */}
-        {/* <PanelSection /> */}
       </Panel>
     );
   });
+// .add("Panel - Outer Wrapper", () => {
+//   return <Panel />;
+// })
+// .add("Panel - 1 Section (default)", () => {
+//   return (
+//     <Panel>
+//       <PanelSection body />
+//     </Panel>
+//   );
+// })
+// .add("Panel - 2 Sections", () => {
+//   return (
+//     <Panel>
+//       <PanelSection />
+//       <PanelSection body />
+//     </Panel>
+//   );
+// })
+// .add("Panel - 3 Sections", () => {
+//   return (
+//     <Panel>
+//       <PanelSection />
+//       <PanelSection body />
+//       <PanelSection />
+//     </Panel>
+//   );
+// })
+// .add("Panel - 4 Sections (2 body)", () => {
+//   return (
+//     <Panel>
+//       <PanelSection />
+//       <PanelSection body />
+//       <PanelSection body />
+//       <PanelSection />
+//     </Panel>
+//   );
+// })
+// .add("Panel - 5 Sections (2 body)", () => {
+//   return (
+//     <Panel>
+//       <PanelSection />
+//       <PanelSection body />
+//       <PanelSection />
+//       <PanelSection body />
+//       <PanelSection />
+//     </Panel>
+//   );
+// })
