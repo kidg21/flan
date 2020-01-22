507167501e1c4a589215ed2c2e56c1ddb950ed3c
derekktong <dtong@digmap.com>
2020-01-22 15:29:53 -0800

    -add comment for deprecated component

--- src/components/index.js
+++ src/components/index.js
@@ -66,6 +66,7 @@ export { default as Grid } from "./layout/Grid";
 export { default as Layout } from "./layout/Layout";
 export { default as Modal } from "./layout/Modal";
 export { default as Page } from "./layout/Page";
+// panel section is deprecated, but our existing components will break if this is removed.
 export { default as Panel, PanelSection } from "./layout/Panel";
 
 export { default as Expander } from "./utils/Expander";
