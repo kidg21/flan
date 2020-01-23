9cdbd84914719310ab1899804db1679dc0986165
Elizabeth Gallagher <EGallagher@digmap.com>
2020-01-22 18:34:24 -0800

    with examples

--- src/components/layout/Layout/Layout.stories.js
+++ src/components/layout/Layout/Layout.stories.js
@@ -1,7 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-filename-extension */
 import React, { useState } from "react";
-
+import Panel from "layout/Panel";
+import List, { ListItem } from "blocks/List";
 import Layout from "./Layout.jsx";
 
 const LayoutNotes = markdown.require("./Layout.md");
@@ -20,5 +21,40 @@ storiesOf("Layout |App Layout/", module)
     "Documentation",
     () => {
       return <Layout />;
+    },
+  )
+
+  .add(
+    "Example",
+    () => {
+      return (<Layout
+        left={{
+          content: (
+            <Panel >
+              <List interactive>
+                <ListItem
+                  label="Dashboard"
+                  icon="draw"
+                />
+                <ListItem
+                  label="Create"
+                  icon="report"
+                />
+                <ListItem
+                  label="Jobs"
+                  icon="list"
+                />
+                <ListItem
+                  label="RFPs"
+                  icon="user"
+                />
+                <ListItem
+                  label="Tasks"
+                  icon="file"
+                />
+              </List>
+            </Panel>),
+        }}
+      />);
     },
   );
