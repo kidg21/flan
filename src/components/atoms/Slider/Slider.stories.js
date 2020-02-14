55741fe2bf5a7d03639604443f11b988fe2810ac
egallagher <egallagher@digmap.com>
2020-02-14 09:54:29 -0800

    corrected console errors of repeated stories

--- src/components/atoms/Slider/Slider.stories.js
+++ src/components/atoms/Slider/Slider.stories.js
@@ -1,40 +1,40 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable react/jsx-filename-extension */
-import React from "react";
-import { Padding } from "helpers/Display";
-import Form, { Section } from "layout/Form";
-import Slider from "atoms/Slider";
+// /* eslint-disable linebreak-style */
+// /* eslint-disable react/jsx-filename-extension */
+// import React from "react";
+// import { Padding } from "helpers/Display";
+// import Form, { Section } from "layout/Form";
+// import Slider from "atoms/Slider";
 
 
 
-storiesOf("Atoms|Slider", module)
-  .addDecorator(Padding)
-  .addDecorator(withKnobs)
-  .add(
-    "Documentation",
-    () => {
-      return <Slider />;
-    })
-  .add("Knobs", () => {
-    return (
-      <Slider
-        error={boolean("error", false, "Slider")}
-        disabled={boolean("disabled", false, "Slider")}
-      />
-    );
-  })
-  .add("States", () => {
-    return (
-      <Form title="States" columns="1">
-        <Section title="Standard">
-          <Slider />
-        </Section>
-        <Section title="Error">
-          <Slider error />
-        </Section>
-        <Section title="Disabled">
-          <Slider disabled />
-        </Section>
-      </Form>
-    );
-  });
+// storiesOf("Atoms|Slider", module)
+//   .addDecorator(Padding)
+//   .addDecorator(withKnobs)
+//   .add(
+//     "Documentation",
+//     () => {
+//       return <Slider />;
+//     })
+//   .add("Knobs", () => {
+//     return (
+//       <Slider
+//         error={boolean("error", false, "Slider")}
+//         disabled={boolean("disabled", false, "Slider")}
+//       />
+//     );
+//   })
+//   .add("States", () => {
+//     return (
+//       <Form title="States" columns="1">
+//         <Section title="Standard">
+//           <Slider />
+//         </Section>
+//         <Section title="Error">
+//           <Slider error />
+//         </Section>
+//         <Section title="Disabled">
+//           <Slider disabled />
+//         </Section>
+//       </Form>
+//     );
+//   });
--- src/components/atoms/Switch/Switch.stories.js
+++ src/components/atoms/Switch/Switch.stories.js
@@ -1,55 +1,55 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable react/jsx-filename-extension */
-import React from "react";
-import { Padding } from "helpers/Display";
-import Form from "layout/Form";
-import Switch from "atoms/Switch";
+// /* eslint-disable linebreak-style */
+// /* eslint-disable react/jsx-filename-extension */
+// import React from "react";
+// import { Padding } from "helpers/Display";
+// import Form from "layout/Form";
+// import Switch from "atoms/Switch";
 
 
-storiesOf("Atoms|Switch", module)
+// storiesOf("Atoms|Switch", module)
 
-  .addDecorator(Padding)
-  .addDecorator(withKnobs)
-  .add(
-    "Documentation",
-    () => {
-      return <Switch label="Switch" />;
-    })
-  .add("Knobs", () => {
-    return (
-      <Switch
-        id="switch1"
-        label={text("label", "Switch Label", "Switch")}
-        align={options(
-          "align",
-          {
-            left: null,
-            right: "right",
-          },
-          null,
-          { display: "select" },
-          "Switch",
-        )}
-        error={boolean("error", false, "Switch")}
-        disabled={boolean("disabled", false, "Switch")}
-      />
-    );
-  })
-  .add("States", () => {
-    return (
-      <Form>
-        <Switch id="inactive" label="Inactive" />
-        <Switch id="active" label="Active" checked />
-        <Switch id="error" label="Error" error />
-        <Switch id="disabled" label="Disabled" disabled />
-      </Form>
-    );
-  })
-  .add("Alignment", () => {
-    return (
-      <Form>
-        <Switch id="standard" label="Standard" />
-        <Switch id="align-right" label="Right Aligned" align="right" />
-      </Form>
-    );
-  });
+//   .addDecorator(Padding)
+//   .addDecorator(withKnobs)
+//   .add(
+//     "Documentation",
+//     () => {
+//       return <Switch label="Switch" />;
+//     })
+//   .add("Knobs", () => {
+//     return (
+//       <Switch
+//         id="switch1"
+//         label={text("label", "Switch Label", "Switch")}
+//         align={options(
+//           "align",
+//           {
+//             left: null,
+//             right: "right",
+//           },
+//           null,
+//           { display: "select" },
+//           "Switch",
+//         )}
+//         error={boolean("error", false, "Switch")}
+//         disabled={boolean("disabled", false, "Switch")}
+//       />
+//     );
+//   })
+//   .add("States", () => {
+//     return (
+//       <Form>
+//         <Switch id="inactive" label="Inactive" />
+//         <Switch id="active" label="Active" checked />
+//         <Switch id="error" label="Error" error />
+//         <Switch id="disabled" label="Disabled" disabled />
+//       </Form>
+//     );
+//   })
+//   .add("Alignment", () => {
+//     return (
+//       <Form>
+//         <Switch id="standard" label="Standard" />
+//         <Switch id="align-right" label="Right Aligned" align="right" />
+//       </Form>
+//     );
+//   });
