5718fe4c42eccd165d9798a129ba988e7b752199
brodarte <brodarte@digmap.com>
2020-02-21 11:11:08 -0800

    Implement content parsing in GenericSurface before content is passed down the presentational template

--- src/components/blocks/GenericSurface/GenericSurface.jsx
+++ src/components/blocks/GenericSurface/GenericSurface.jsx
@@ -1,40 +1,55 @@
 import React from "react";
-import SummaryPanel from "./SurfaceTemplates/SummaryPanelTemplate.jsx";
 import PropTypes from "prop-types";
+import SummaryPanel from "./SurfaceTemplates/SummaryPanelTemplate.jsx";
 
 const templateComponents = {
   SummaryPanel,
 };
 
-// const interpolate = function(params) {
-//   const names = Object.keys(params);
-//   const vals = Object.values(params);
-//   return new Function(...names, `return \`${this}\`;`)(...vals);
-// }
-
-// const template = 'Example text: ${text}';
-// const result = template.interpolate({
-//   text: 'Foo Boo'
-// });
-
-const interpolate = (template, records) => {
-  const funcBody = `"use strict"; return (function (records, numberFormatter) { return \`${template.value}\`})`;
-  // eslint doesn't like this, but it's better than eval according to MDN:
+const interpolate = (templateString, records) => {
+  const funcBody = `"use strict"; return (function (records) { return \`${templateString}\`})`;
+  // eslint doesn't like this, but it's at least better than eval according to MDN so ¯\_(ツ)_/¯:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#Never_use_eval!
   // eslint-disable-next-line no-new-func
-  const output = Function(`${funcBody}`)()(records, Intl.NumberFormat);
+  const output = Function(`${funcBody}`)()(records);
   return output;
+};
+
+const parseContent = (rawContent) => {
+  const parsedSections = rawContent.sections.map((rawSection) => {
+    // parse the raw display fields (values converted from template strings to literal values)
+    const parsedDisplayFields = rawSection.displayFields.map((rawDisplayField) => {
+      return {
+        ...rawDisplayField,
+        value: interpolate(rawDisplayField.value, rawSection.records),
+      };
+    });
+
+    // parse the raw commands
+    const parsedCommands = rawSection.commands.map((rawCommand) => {
+      // not doing anything here right now, but later we may pass in Command components
+      // or need to instantiate command objects here before passing into presentational template
+      return {
+        ...rawCommand,
+      };
+    });
+
+    // return the new section with the displayFields and commands replaced by their parsed versions
+    return {
+      ...rawSection,
+      displayFields: parsedDisplayFields,
+      commands: parsedCommands,
+    };
+  });
+  return {
+    ...rawContent,
+    sections: parsedSections,
+  };
 };
 
 const GenericSurface = (props) => {
   const Template = templateComponents[props.templateName || "SummaryPanel"];
-  const parsedContent = props.content[0].displayFields.map((field) => {
-    return {
-      ...field,
-      value: interpolate(field, props.content[0].records),
-    };
-  });
-  return (<Template message={"Hello there"} content={parsedContent} />);
+  return (<Template content={parseContent(props.content)} />);
 };
 
 GenericSurface.propTypes = {
@@ -42,16 +57,19 @@ GenericSurface.propTypes = {
   content: PropTypes.shape({
     title: PropTypes.string.isRequired,
     sections: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequied,
-      data: PropTypes.arrayOf(PropTypes.shape({
-        key: PropTypes.string,
-        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      title: PropTypes.string.isRequired,
+      records: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+      displayFields: PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
         onClick: PropTypes.func,
-      })).isRequired,
+      })),
       commands: PropTypes.arrayOf(PropTypes.shape({
-
-      })).isRequired,
-    })),
+        label: PropTypes.string.isRequired,
+        icon: PropTypes.string,
+        onClick: PropTypes.func.isRequired,
+      })),
+    })).isRequired,
   }).isRequired,
 };
 
--- src/components/blocks/GenericSurface/SurfaceTemplates/SummaryPanelTemplate.jsx
+++ src/components/blocks/GenericSurface/SurfaceTemplates/SummaryPanelTemplate.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default (props) => {
-  return (props.content.map((field) => {
-    return (<div>key: {field.label} -- value: {field.value}</div>);
-  }));
+const SummaryPanelTemplate = (props) => {
+  return (
+    <div>
+      <h2>{props.content.title}</h2>
+      <br />
+      {props.content.sections.map((section) => {
+        return (
+          <div>
+            <h4>{section.title}</h4>
+            {section.displayFields.map((displayField) => {
+              let displayFieldValue = displayField.value;
+              if (displayField.onClick) {
+                displayFieldValue = (
+                  <button onClick={displayField.onClick}>
+                    {displayField.value}
+                  </button>);
+              }
+              return (
+                <div>{displayField.label} : {displayFieldValue}</div>
+              );
+            })}
+            Commands:
+            {section.commands.map((command) => {
+              return (<button onClick={command.onClick}>{command.label}</button>);
+            })}
+            <br /><br />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
+
+SummaryPanelTemplate.propTypes = {
+  content: PropTypes.shape({
+    title: PropTypes.string,
+    sections: PropTypes.arrayOf(PropTypes.shape({
+      title: PropTypes.string,
+      displayFields: PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+        onClick: PropTypes.func,
+      })),
+      commands: PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        icon: PropTypes.string,
+        onClick: PropTypes.func.isRequired,
+      })),
+    })),
+  }).isRequired,
+};
+
+export default SummaryPanelTemplate;
