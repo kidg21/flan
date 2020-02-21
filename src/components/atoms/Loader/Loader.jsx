70ead68d83a56d397339812b2ba72e26f01b80d5
Greg Person <gperson@digmap.com>
2020-02-21 15:16:29 -0800

    Loader - Linting

--- src/components/atoms/Loader/Loader.jsx
+++ src/components/atoms/Loader/Loader.jsx
@@ -1,34 +1,31 @@
+/* eslint-disable linebreak-style */
 import React, { Fragment } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-
 const Fill = styled.div`
-height: 0.65rem;
-position: relative;
-width: 100px;
-animation: slide 1.5s linear infinite;
-background: ${(props) => {
+  height: 0.65rem;
+  position: relative;
+  width: 100px;
+  animation: slide 1.5s linear infinite;
+  background: ${(props) => {
     return props.theme.palette.action80;
   }};
-
-
   @keyframes slide
   {
-      0%   {width: 0;}
-      100% {width: 100%;}
+    0%   {width: 0;}
+    100% {width: 100%;}
   }
 `;
 
-
 const Chase = styled.div`
-width: 100%;
-height: 3rem;
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-position: relative;
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 `;
 
 const Dot = styled.div`
@@ -37,10 +34,6 @@ const Dot = styled.div`
   margin: 0.8rem;
   width: 0.8rem;
   border-radius: 50%;
-
-
-  
-  
   &:nth-child(1) {
     animation: ld4 4s linear infinite 0s; }
   &:nth-child(2) {
@@ -49,36 +42,36 @@ const Dot = styled.div`
     animation: ld4 4s linear infinite 0.3s; }
   &:nth-child(4) {
     animation: ld4 4s linear infinite 0.45s; }
-
-@keyframes ld4 {
-  0% {
-    opacity: 0;
-    transform: scale(0.5);
-    background: ${(props) => {
+  @keyframes ld4 {
+    0% {
+      opacity: 0;
+      transform: scale(0.5);
+      background: ${(props) => {
     return props.theme.palette.action80;
   }}; }
-  25% {
-    opacity: 1;
-    transform: scale(1.5);
-    background: ${(props) => {
+    25% {
+      opacity: 1;
+      transform: scale(1.5);
+      background: ${(props) => {
     return props.theme.palette.action80;
   }}; }
-  50% {
-    opacity: 0;
-    transform: scale(0.5);
-    background: ${(props) => {
+    50% {
+      opacity: 0;
+      transform: scale(0.5);
+      background: ${(props) => {
     return props.theme.palette.action80;
   }}; }
-  75% {
-    opacity: 1;
-    transform: scale(1.5);
-    background: ${(props) => {
+    75% {
+      opacity: 1;
+      transform: scale(1.5);
+      background: ${(props) => {
     return props.theme.palette.action80;
   }}; }
-  100% {
-    opacity: 0;
+    100% {
+      opacity: 0;
+    }
   }
-  `;
+`;
 
 function Loader({ line, id }) {
   let content;
@@ -103,7 +96,6 @@ function Loader({ line, id }) {
   );
 }
 
-
 Loader.propTypes = {
   id: PropTypes.string,
   line: PropTypes.cool,
@@ -113,7 +105,6 @@ Loader.defaultProps = {
   id: null,
   line: null,
 };
-
 
 export default Loader;
 
