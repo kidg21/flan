a83a31da0d5f176fa4f6d7d64b538cc2fe0b0d6f
Greg Person <gperson@digmap.com>
2020-02-26 12:40:05 -0800

    Data Range - Semantic review - Alphabetized props - Changed Bar alignment from 'center' to 'bottom' to account for sections without labels - Added missing 'error' props to Select Menus - Structured the 'select' prop like 'min' and 'max' - Updated knobs stories

--- src/components/blocks/DataRange/DataRange.jsx
+++ src/components/blocks/DataRange/DataRange.jsx
@@ -27,23 +27,9 @@ function DataRange({
   id,
   isRequired,
   label,
-  // labelMax,
-  // maxValue,
-  // optionsMax,
-  // disableRight,
-  // labelMin,
-  // minValue,
-  // onChange,
-  // optionsMin,
-  // disableLeft,
-  // withSelector,
-  // onChangeSelector,
-  // optionsSelect,
-  // optionsSelectedOptions,
-
-  select,
   max,
   min,
+  select,
 }) {
   let inputTextColor;
   const isDisabled =
@@ -66,67 +52,70 @@ function DataRange({
   const uId = id || getGuid();
   return (
     <RangeContainer
-      id={uId}
-      disabled={isDisabled}
-      inputTextColor={inputTextColor}
       columns="1"
+      disabled={isDisabled}
       gap="tiny"
+      id={uId}
+      inputTextColor={inputTextColor}
     >
       {label ? (
         <Label size="2x" isRequired={isRequired} text={label} />
       ) : null}
       <Bar
         padding="none"
-        contentAlign="center"
+        contentAlign="bottom"
         left={
           min.options ? (
             <SelectMenu
+              disabled={min.disabled || isDisabled}
+              error={!!error}
               id={`${uId}_left`}
               label={min.label}
-              options={min.options}
               onChangeState={onChangeMin}
-              disabled={min.disabled || isDisabled}
+              options={min.options}
               selectOptions={min.value}
             />)
             :
             (<TextInput
+              disabled={min.disabled || isDisabled}
+              error={!!error}
               id={`${uId}_left`}
               label={min.label}
               onChange={onChangeMin}
-              error={!!error}
-              disabled={min.disabled || isDisabled}
               value={min.value}
             />)
         }
         center={
-          select ? (
+          select.options ? (
             <SelectMenu
+              disabled={select.disabled || isDisabled}
+              error={!!error}
               id={`${uId}_center`}
-              options={select.options}
               label={select.label}
               onChangeState={select.onChange}
+              options={select.options}
               selectOptions={select.selected}
-              disabled={isDisabled}
             />
           ) : null
         }
         right={
           max.options ? (
             <SelectMenu
+              disabled={max.disabled || isDisabled}
+              error={!!error}
               id={`${uId}_right`}
               label={max.label}
-              options={max.options}
               onChangeState={max.onChange}
-              disabled={max.disabled || isDisabled}
+              options={max.options}
               selectOptions={max.selected}
             />)
             :
             (<TextInput
+              disabled={max.disabled || isDisabled}
+              error={!!error}
               id={`${uId}_right`}
               label={max.label}
               onChange={onChangeMax}
-              error={!!error}
-              disabled={max.disabled || isDisabled}
               value={max.value}
             />)
         }
@@ -140,19 +129,19 @@ function DataRange({
 }
 
 const selectType = PropTypes.shape({
-  options: PropTypes.map,
-  selected: PropTypes.arrayOf(PropTypes.object),
-  onChange: PropTypes.func,
   disabled: PropTypes.bool,
   label: PropTypes.string,
+  onChange: PropTypes.func,
+  options: PropTypes.map,
+  selected: PropTypes.arrayOf(PropTypes.object),
 });
 
 const textType = PropTypes.shape({
-  value: PropTypes.string,
-  label: PropTypes.string,
-  error: PropTypes.bool,
   disabled: PropTypes.bool,
+  error: PropTypes.bool,
+  label: PropTypes.string,
   onChange: PropTypes.func,
+  value: PropTypes.string,
 });
 
 DataRange.propTypes = {
@@ -164,7 +153,8 @@ DataRange.propTypes = {
   label: PropTypes.string,
   max: PropTypes.oneOfType([textType, selectType]),
   min: PropTypes.oneOfType([textType, selectType]),
-  select: selectType,
+  select: PropTypes.oneOfType([textType, selectType]),
+  // select: selectType,
 };
 DataRange.defaultProps = {
   disabled: false,
@@ -173,9 +163,9 @@ DataRange.defaultProps = {
   id: null,
   isRequired: false,
   label: null,
-  select: null,
   max: {},
   min: {},
+  select: {},
 };
 
 export default DataRange;
--- src/components/blocks/DataRange/DataRange.stories.js
+++ src/components/blocks/DataRange/DataRange.stories.js
@@ -6,7 +6,6 @@ import Form, { Section } from "layout/Form";
 import DataRange from "blocks/DataRange";
 
 
-
 const options = [
   { value: "0", label: "0" },
   { value: "1", label: "1" },
@@ -59,25 +58,29 @@ storiesOf("Blocks|Data Range", module)
           />
         </Form>
       );
-    })
+    },
+  )
   .add("Knobs", () => {
     return (
       <DataRange
         id="knobs"
-        label={text("label", "Data Label", "Data Range")}
-        labelMin={text("min label", "Min", "Data Range")}
-        optionsMin={object(
-          "options (min)",
-          [{ value: "0", label: "0" }],
-          "Data Range",
-        )}
-        labelMax={text("max label", "Max", "Data Range")}
-        optionsMax={object(
-          "options (max)",
-          [{ value: "0", label: "0" }],
-          "Data Range",
-        )}
+        label={text("input label", "Data Label", "Data Range")}
         isRequired={boolean("required", false, "Data Range")}
+        min={{
+          label: text("min label", "Min", "Data Range"),
+          options: boolean("select (min)", false, "Data Range") &&
+            object("min values", options, "Data Range"),
+        }}
+        select={{
+          label: text("center label", "Operator", "Data Range"),
+          options: boolean("operator", false, "Data Range") &&
+            object("operator values", selectorOptions, "Data Range"),
+        }}
+        max={{
+          label: text("max label", "Max", "Data Range"),
+          options: boolean("select (max)", false, "Data Range") &&
+            object("max values", options, "Data Range"),
+        }}
         helpText={text("help text", "Help text goes here...", "Data Range")}
         error={
           boolean("error", false, "Data Range") &&
@@ -94,23 +97,35 @@ storiesOf("Blocks|Data Range", module)
           <DataRange
             id="input"
             label="Input Range"
-            labelMin="Min"
-            labelMax="Max"
+            min={{
+              label: "Min",
+            }}
+            max={{
+              label: "Max",
+            }}
             helpText="Hang in there, buddy, I'm here to help!"
           />
           <DataRange
             id="inputError"
             label="Input Range (error)"
-            labelMin="Min"
-            labelMax="Max"
+            min={{
+              label: "Min",
+            }}
+            max={{
+              label: "Max",
+            }}
             helpText="Hang in there, buddy, I'm here to help!"
             error="Out of range!"
           />
           <DataRange
             id="inputDisabled"
             label="Input Range (disabled)"
-            labelMin="Min"
-            labelMax="Max"
+            min={{
+              label: "Min",
+            }}
+            max={{
+              label: "Max",
+            }}
             helpText="Hang in there, buddy, I'm here to help!"
             disabled
           />
@@ -119,39 +134,55 @@ storiesOf("Blocks|Data Range", module)
           <DataRange
             id="select"
             label="Select Range"
-            labelMin="Min"
-            optionsMin={options}
-            labelMax="Max"
-            optionsMax={options}
+            min={{
+              label: "Min",
+              options: options,
+            }}
+            max={{
+              label: "Max",
+              options: options,
+            }}
             helpText="Hang in there, buddy, I'm here to help!"
           />
           <DataRange
             id="selectOperation"
             label="Select Range"
-            labelMin="Min"
+            min={{
+              label: "Min",
+              options: options,
+            }}
             select={{ options: selectorOptions }}
-            optionsMin={options}
-            labelMax="Max"
-            optionsMax={options}
+            max={{
+              label: "Max",
+              options: options,
+            }}
             helpText="This help text has been passed through a prop!"
           />
           <DataRange
             id="selectError"
             label="Select Range (error)"
-            labelMin="Min"
-            optionsMin={options}
-            labelMax="Max"
-            optionsMax={options}
+            min={{
+              label: "Min",
+              options: options,
+            }}
+            max={{
+              label: "Max",
+              options: options,
+            }}
             helpText="Hang in there, buddy, I'm here to help!"
             error="Out of range!"
           />
           <DataRange
             id="selectDisabled"
             label="Select Range (disabled)"
-            labelMin="Min"
-            optionsMin={options}
-            labelMax="Max"
-            optionsMax={options}
+            min={{
+              label: "Min",
+              options: options,
+            }}
+            max={{
+              label: "Max",
+              options: options,
+            }}
             helpText="Hang in there, buddy, I'm here to help!"
             disabled
           />
