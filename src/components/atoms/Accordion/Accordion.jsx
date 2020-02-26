433bc5953e3d329f9b684f282d5f6c6424efe17a
egallagher <egallagher@digmap.com>
2020-02-26 13:58:01 -0800

    half of code review comments

--- src/components/atoms/Accordion/Accordion.jsx
+++ src/components/atoms/Accordion/Accordion.jsx
@@ -1,27 +1,27 @@
 /* eslint-disable linebreak-style */
-import React, { Fragment, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Bar from "blocks/Bar";
 import Icon from "atoms/Icon";
 import Expander from "utils/Expander";
-import Text, {Title} from "base/Typography";
+import Text, { Title } from "base/Typography";
 
 
 function Accordion({
-    children, text, id,  onClick, open, title,
-  }) {
-    let rotation;
-    if (open) {
-      rotation = 180;
-    } else {
-      rotation = 0;
-    }
-    return (
-      <Expander
-        id={id}
-        onClick={onClick}
-        open={open}
-        header={
+  children, description, id, onClick, open, title,
+}) {
+  let rotation;
+  if (open) {
+    rotation = 180;
+  } else {
+    rotation = 0;
+  }
+  return (
+    <Expander
+      id={id}
+      onClick={onClick}
+      open={open}
+      header={
           title || text ? (
             <Bar
               contentAlign="center"
@@ -29,38 +29,34 @@ function Accordion({
               left={
                 <React.Fragment>
                   {title ? <Title text={title} /> : null}
-                  {text ? <Text text={text} /> : null}
+                  {text ? <Text text={description} /> : null}
                 </React.Fragment>
               }
               rightWidth="max-content"
               right={children ? <Icon icon="up" rotation={rotation} /> : null}
             />
           ) : null}
-      >
-        {children}
-      </Expander>
-    );
-  }
-  
-  Accordion.propTypes = {
-    children: PropTypes.node,
-    text: PropTypes.string,
-    icon: PropTypes.string,
-    id: PropTypes.string,
-    label: PropTypes.string,
-    onClick: PropTypes.func,
-    open: PropTypes.bool,
-    title: PropTypes.string,
-  };
-  Accordion.defaultProps = {
-    children: null,
-    text: null,
-    icon: null,
-    id: null,
-    label: null,
-    onClick: null,
-    open: false,
-    title: null,
-  };
+    >
+      {children}
+    </Expander>
+  );
+}
 
-  export default Accordion;
+Accordion.propTypes = {
+  children: PropTypes.node,
+  description: PropTypes.string,
+  id: PropTypes.string,
+  onClick: PropTypes.func,
+  open: PropTypes.bool,
+  title: PropTypes.string,
+};
+Accordion.defaultProps = {
+  children: null,
+  description: null,
+  id: null,
+  onClick: null,
+  open: false,
+  title: null,
+};
+
+export default Accordion;
--- src/components/atoms/Accordion/Accordion.stories.js
+++ src/components/atoms/Accordion/Accordion.stories.js
@@ -1,12 +1,12 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-filename-extension */
-import React, {useState} from "react";
-import Accordion from "./Accordion.jsx";
+import React, { useState } from "react";
 import Text from "base/Typography";
+import Accordion from "./Accordion.jsx";
 
 storiesOf("Atoms|Accordion", module)
   .addDecorator(withKnobs)
-  .add("Default",() => {
+  .add("Default", () => {
     const [open, setOpen] = useState(false);
     function toggleDropdown() {
       if (open) {
@@ -15,39 +15,37 @@ storiesOf("Atoms|Accordion", module)
         setOpen(true);
       }
     }
-      return (
-        <Accordion
+    return (
+      <Accordion
         onClick={(e) => {
           toggleDropdown(e);
-          if (more.onToggle) more.onToggle(e);
         }}
         open={open}
         title="Accordion Title"
       >
-        <Text text="hi"/>
+        <Text text="hi" />
       </Accordion>
-      );
-    })
-    .add("Text",() => {
-        const [open, setOpen] = useState(false);
-        function toggleDropdown() {
-          if (open) {
-            setOpen(false);
-          } else {
-            setOpen(true);
-          }
-        }
-          return (
-            <Accordion
-            onClick={(e) => {
+    );
+  })
+  .add("Text", () => {
+    const [open, setOpen] = useState(false);
+    function toggleDropdown() {
+      if (open) {
+        setOpen(false);
+      } else {
+        setOpen(true);
+      }
+    }
+    return (
+      <Accordion
+        onClick={(e) => {
               toggleDropdown(e);
-              if (more.onToggle) more.onToggle(e);
             }}
-            open={open}
-            text="Accordion Title"
-          >
-            <Text text="hi"/>
-          </Accordion>
-          );
-        });
-    
+        open={open}
+        description="Accordion Title"
+      >
+        <Text text="hi" />
+      </Accordion>
+    );
+  });
+
