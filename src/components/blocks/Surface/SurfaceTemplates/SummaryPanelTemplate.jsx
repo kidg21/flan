26ac0155891906ffc2ad5c336056848d461a3e10
brodarte <brodarte@digmap.com>
2020-03-10 14:44:26 -0700

    Remove unused imports

--- src/components/blocks/Surface/SurfaceTemplates/SummaryPanelTemplate.jsx
+++ src/components/blocks/Surface/SurfaceTemplates/SummaryPanelTemplate.jsx
@@ -1,70 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import Panel from "layout/Panel";
-import Command from "atoms/Command";
-import Menu from "blocks/Menu";
 import Legend from "blocks/Legend";
-import Form, { Section } from "layout/Form";
 import Divider from "atoms/Divider";
-import Text, { Title, Link } from "base/Typography";
-import Grid from "layout/Grid";
+import { Title } from "base/Typography";
 import MainPanelHeader from "elements/PanelHeaders/MainPanelHeader";
-
-
-const TableContainer = styled.table`
-  width: 100%;
-  color: ${(props) => {
-    return props.theme.text.primary;
-  }};
-  table-layout: fixed;
-  border-collapse: collapse;
-  min-width: 400px;
-`;
-
-const Row = styled.tr`
-  margin: 1em;
-`;
-
-const Cell = styled.td`
-  padding: 0.25em;
-  `;
-
-/*
-content passed in via props will look like this:
-{
-  title: "The title of the surface",
-  sections: [
-    {
-      title: "The title of the first section",
-      displayFields: [
-        {
-          label: "The label of a key-val pair in section 1",
-          value: "The value of a key-val pair in section 1",
-          onClick: () => { // a func to be executed when the user clicks on this field},
-        }
-        {
-          // another field
-        }
-      ],
-      commands: [
-        {
-          label: "A command to be included in the 1st section",
-          icon: "an icon to use -- not sure on format yet...",
-          onClick: () => { // a func to be executed when the user clicks on this command },
-        },
-        {
-          // another command
-        }
-      ]
-    },
-    {
-      // another section
-    }
-  ]
-}
-*/
 
 // The following component is just to show how to iterate through all the data
 const SummaryPanelTemplate = (props) => {
