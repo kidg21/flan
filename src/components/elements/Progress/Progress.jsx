cbc6e4d9120bc076f82dd4f5b77dc511efd7d2e4
Greg Person <gperson@digmap.com>
2020-03-19 16:41:39 -0700

    Progress - Semantic review - Alphabetized props
    Breaking Changes
    - Changed 'text' prop to 'title'

--- src/components/elements/Progress/Progress.jsx
+++ src/components/elements/Progress/Progress.jsx
@@ -1,21 +1,22 @@
+/* eslint-disable linebreak-style */
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import Text, { Title } from "base/Typography";
 
 const StepItem = styled.li`
-align-items: center;
-align-self: center;
-vertical-align: center;
-font-family: Nunito;
-justify-content: center;
-padding-bottom: 0.5rem;
-display: flex;
-border-bottom: 2px solid ${(props) => { return props.success ? props.theme.palette.neutral40 : props.isSelected ? props.theme.palette.selected : props.theme.palette.neutral40; }};
-align-items: baseline;
-width: 100%;
-color: ${(props) => { return props.success ? props.theme.text.secondary : props.isSelected ? props.theme.palette.selected : props.theme.text.secondary; }};
-&:before {
+  align-items: center;
+  align-self: center;
+  vertical-align: center;
+  font-family: Nunito;
+  justify-content: center;
+  padding-bottom: 0.5rem;
+  display: flex;
+  border-bottom: 2px solid ${(props) => { return props.success ? props.theme.palette.neutral40 : props.isSelected ? props.theme.palette.selected : props.theme.palette.neutral40; }};
+  align-items: baseline;
+  width: 100%;
+  color: ${(props) => { return props.success ? props.theme.text.secondary : props.isSelected ? props.theme.palette.selected : props.theme.text.secondary; }};
+  &:before {
     content: counter(step);
     counter-increment: step;
     height: 1.5rem;
@@ -31,83 +32,71 @@ color: ${(props) => { return props.success ? props.theme.text.secondary : props.
     border: 1px solid ${(props) => { return props.success ? props.theme.palette.success80 : props.isSelected ? props.theme.palette.selected : props.theme.text.secondary; }};
     border-radius: 50%;
     font-weight: 600;
-}
-
-
+  }
 `;
-
 
 const Container = styled.ul`
-display: flex;
-list-style: none;
-counter-reset: step;
-align-items: flex-start;
-width: 100%;
-justify-content: space-between;
-z-index: 1;
-
-    `;
-
-
-const ItemContainer = styled.div`
-display: flex;
-justify-content: ${(props) => { return props.line ? "space-between" : ""; }};
-flex-direction: column;
-width: ${(props) => { return props.line ? "100%" : ""; }};
-
+  display: flex;
+  list-style: none;
+  counter-reset: step;
+  align-items: flex-start;
+  width: 100%;
+  justify-content: space-between;
+  z-index: 1;
 `;
 
+const ItemContainer = styled.div`
+  display: flex;
+  justify-content: ${(props) => { return props.line ? "space-between" : ""; }};
+  flex-direction: column;
+  width: ${(props) => { return props.line ? "100%" : ""; }};
+`;
 
 function Step({
-    id, success, text, description, isSelected,
+  description, id, isSelected, success, title,
 }) {
-    return (
-        <StepItem id={id} success={success} isSelected={isSelected}>
-            <ItemContainer>
-                <Title text={text} />
-                {description ? (<Text text={description} />
-                ) : null}
-            </ItemContainer>
-        </StepItem >
-
-    );
-}
-
-
-function ProgressBar({
-    id, children,
-}) {
-    return (
-        <Container id={id}>
-            {children}
-        </Container>
-    );
+  return (
+    <StepItem id={id} success={success} isSelected={isSelected}>
+      <ItemContainer>
+        <Title text={title} size="2x" weight="bold" />
+        {description ? <Text text={description} /> : null}
+      </ItemContainer>
+    </StepItem>
+  );
 }
 
 Step.propTypes = {
-    id: PropTypes.string,
-    success: PropTypes.bool,
-    description: PropTypes.string,
-    text: PropTypes.string,
-    isSelected: PropTypes.bool,
+  description: PropTypes.string,
+  id: PropTypes.string,
+  isSelected: PropTypes.bool,
+  success: PropTypes.bool,
+  title: PropTypes.string,
 };
+Step.defaultProps = {
+  description: null,
+  id: null,
+  isSelected: false,
+  success: false,
+  title: null,
+};
+
+function ProgressBar({
+  children, id,
+}) {
+  return (
+    <Container id={id}>
+      {children}
+    </Container>
+  );
+}
 
 ProgressBar.propTypes = {
-    id: PropTypes.string,
-    children: PropTypes.node,
+  children: PropTypes.node,
+  id: PropTypes.string,
 };
-
-Step.defaultProps = {
-    id: null,
-    success: false,
-    description: null,
-    text: null,
-    isSelected: false,
-};
-
 ProgressBar.defaultProps = {
-    id: null,
-    children: null,
+  children: null,
+  id: null,
 };
 
 export { ProgressBar as default, Step };
--- src/components/elements/Progress/Progress.stories.js
+++ src/components/elements/Progress/Progress.stories.js
@@ -8,15 +8,15 @@ import { Padding } from "helpers/Display";
 import ProgressBar, { Step } from "./Progress.jsx";
 
 storiesOf("Elements|Progress", module)
-    .addDecorator(Padding)
-    .add("Flow", () => {
-        return (
-            <ProgressBar >
-                <Step success text="Task" description="this is what you do" />
-                <Step success text="Task" description="this is what you do" />
-                <Step isSelected text="Task" description="this is what you do" />
-                <Step text="Task" description="this is what you do" />
-                <Step text="Task" description="this is what you do" />
-            </ProgressBar>
-        );
-    });
+  .addDecorator(Padding)
+  .add("Flow", () => {
+    return (
+      <ProgressBar >
+        <Step success title="Task" description="this is what you do" />
+        <Step success title="Task" description="this is what you do" />
+        <Step isSelected title="Task" description="this is what you do" />
+        <Step title="Task" description="this is what you do" />
+        <Step title="Task" description="this is what you do" />
+      </ProgressBar>
+    );
+  });
