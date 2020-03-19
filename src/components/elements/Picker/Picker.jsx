80ee4f386c6186962dc82bd41f6c02e65bd24e50
Greg Person <gperson@digmap.com>
2020-03-18 18:10:26 -0700

    Picker - Semantic Review - Alphabetized props - Configured propTypes

--- src/components/elements/Picker/Picker.jsx
+++ src/components/elements/Picker/Picker.jsx
@@ -7,44 +7,44 @@ import Icon from "atoms/Icon";
 import Text from "base/Typography";
 import Grid from "layout/Grid";
 
-
 const Swatch = styled.button`
-width: ${(props) => {
-  return props.width ;
-}};
-height: ${(props) => {
-  return props.height ;
-}};
-justify-content: center;
-display: flex;
-vertical-align: center;
-align-items: center;
-color: ${(props) => {
-  return props.theme.palette.inverse ;
-}};
-border: 0px solid ${(props) => {
-  return props.theme.palette.inverse ;
-}};
-cursor: pointer;
-border-radius: ${(props) => {
-  return props.borderRadius ;
-}};
-background-color: ${(props) => {
-  return props.theme.palette[props.color];
-}};
-&:hover { 
-  ${Darken};
-};
-&:focus {
-  outline: none;
-}
+  width: ${(props) => {
+    return props.width;
+  }};
+  height: ${(props) => {
+    return props.height;
+  }};
+  justify-content: center;
+  display: flex;
+  vertical-align: center;
+  align-items: center;
+  color: ${(props) => {
+    return props.theme.palette.inverse;
+  }};
+  border: 0px solid ${(props) => {
+    return props.theme.palette.inverse;
+  }};
+  cursor: pointer;
+  border-radius: ${(props) => {
+    return props.borderRadius;
+  }};
+  background-color: ${(props) => {
+    return props.theme.palette[props.color];
+  }};
+  &:hover { 
+    ${Darken};
+  };
+  &:focus {
+    outline: none;
+  }
 `;
 
-
-function ColorSwatch({isSelected, color, square, onClick}) {
+function ColorSwatch({
+  color, isSelected, onClick, square,
+}) {
   let borderRadius;
-  let width = "1.5rem";
-  let height = "1.5rem";
+  const width = "1.5rem";
+  const height = "1.5rem";
 
   if (square) {
     borderRadius = "4px";
@@ -52,51 +52,64 @@ function ColorSwatch({isSelected, color, square, onClick}) {
     borderRadius = "100%";
   }
 
-
   return (
-    
     <React.Fragment>
       {isSelected ? (
-       <Swatch
-       borderRadius={borderRadius}
-       width={width}
-       height={height}
-       onClick={onClick}
-       color={color}
-                >
-         <Icon icon="check"/>
-        </Swatch>
-      
-      ) : (
         <Swatch
-        borderRadius={borderRadius}
-        width={width}
-        height={height}
-        onClick={onClick}
-        color={color}
-      />
+          borderRadius={borderRadius}
+          color={color}
+          height={height}
+          onClick={onClick}
+          width={width}
+        >
+          <Icon icon="check" />
+        </Swatch>
+      ) : (
+          <Swatch
+            borderRadius={borderRadius}
+            color={color}
+            height={height}
+            onClick={onClick}
+            width={width}
+          />
         )}
     </React.Fragment>
-  )
+  );
+}
+
+ColorSwatch.propTypes = {
+  color: PropTypes.node,
+  isSelected: PropTypes.bool,
+  onClick: PropTypes.func,
+  square: PropTypes.bool,
+};
+ColorSwatch.defaultProps = {
+  color: null,
+  isSelected: false,
+  onClick: null,
+  square: false,
 };
 
-
-function Picker({children, columns, text
-}) {
-
+function Picker({ children, columns, text }) {
   return (
     <React.Fragment>
-      { text ? <Text text={text} /> : null}
-    <Grid
-      columns={columns}
-    >
-      {children}
-
-    </Grid>
+      {text ? <Text text={text} /> : null}
+      <Grid columns={columns} >
+        {children}
+      </Grid>
     </React.Fragment>
   );
+}
+
+Picker.propTypes = {
+  children: PropTypes.node,
+  columns: PropTypes.string,
+  text: PropTypes.string,
 };
-
-
+Picker.defaultProps = {
+  children: null,
+  columns: null,
+  text: null,
+};
 
 export { Picker as default, ColorSwatch };
--- src/components/elements/Picker/Picker.stories.js
+++ src/components/elements/Picker/Picker.stories.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-filename-extension */
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Padding } from "helpers/Display";
-import Picker, {ColorSwatch} from "./Picker";
+import Picker, { ColorSwatch } from "elements/Picker";
 
 
 storiesOf("Elements|Picker", module)
@@ -12,110 +12,108 @@ storiesOf("Elements|Picker", module)
     () => {
       return (
         <Picker columns="4">
-          <ColorSwatch color="jobs"/>
-          <ColorSwatch color="bi"/>
-          <ColorSwatch isSelected color="research"/>
+          <ColorSwatch color="jobs" />
+          <ColorSwatch color="bi" />
+          <ColorSwatch isSelected color="research" />
           <ColorSwatch color="brand1" />
           <ColorSwatch color="brand2" />
-          <ColorSwatch color="brand3"/>
-          <ColorSwatch color="brand4"/>
+          <ColorSwatch color="brand3" />
+          <ColorSwatch color="brand4" />
         </Picker>
       );
-    })
-    .add("Toggle-Select", () => {
-      return React.createElement(() => {
-        const [activeToggleTab, setActiveToggleTab] = useState("");
-        return (
-          <Picker >
-            <ColorSwatch
+    },
+  )
+  .add("Toggle-Select", () => {
+    return React.createElement(() => {
+      const [activeToggleTab, setActiveToggleTab] = useState("");
+      return (
+        <Picker >
+          <ColorSwatch
             color="jobs"
-              isSelected={activeToggleTab === "tab1"}
-              onClick={() => {
-                if (activeToggleTab === "tab1") {
-                  setActiveToggleTab("");
-                } else {
-                  setActiveToggleTab("tab1");
-                }
-                return false;
-              }}
-            />
-            <ColorSwatch
+            isSelected={activeToggleTab === "tab1"}
+            onClick={() => {
+              if (activeToggleTab === "tab1") {
+                setActiveToggleTab("");
+              } else {
+                setActiveToggleTab("tab1");
+              }
+              return false;
+            }}
+          />
+          <ColorSwatch
             color="research"
-              isSelected={activeToggleTab === "tab2"}
-              onClick={() => {
-                if (activeToggleTab === "tab2") {
-                  setActiveToggleTab("");
-                } else {
-                  setActiveToggleTab("tab2");
-                }
-                return false;
-              }}
-            />
-            <ColorSwatch
+            isSelected={activeToggleTab === "tab2"}
+            onClick={() => {
+              if (activeToggleTab === "tab2") {
+                setActiveToggleTab("");
+              } else {
+                setActiveToggleTab("tab2");
+              }
+              return false;
+            }}
+          />
+          <ColorSwatch
             color="bi"
-              isSelected={activeToggleTab === "tab3"}
-              onClick={() => {
-                if (activeToggleTab === "tab3") {
-                  setActiveToggleTab("");
-                } else {
-                  setActiveToggleTab("tab3");
-                }
-                return false;
-              }}
-            />
-          </Picker>
-        );
-      });
-    })
-    
-    .add("Square ", () => {
-      return React.createElement(() => {
-        const [activeToggleTab, setActiveToggleTab] = useState("");
-        return (
-          <Picker >
-            <ColorSwatch
-            square
+            isSelected={activeToggleTab === "tab3"}
+            onClick={() => {
+              if (activeToggleTab === "tab3") {
+                setActiveToggleTab("");
+              } else {
+                setActiveToggleTab("tab3");
+              }
+              return false;
+            }}
+          />
+        </Picker>
+      );
+    });
+  })
 
+  .add("Square ", () => {
+    return React.createElement(() => {
+      const [activeToggleTab, setActiveToggleTab] = useState("");
+      return (
+        <Picker >
+          <ColorSwatch
+            square
             color="jobs"
-              isSelected={activeToggleTab === "tab1"}
-              onClick={() => {
-                if (activeToggleTab === "tab1") {
-                  setActiveToggleTab("");
-                } else {
-                  setActiveToggleTab("tab1");
-                }
-                return false;
-              }}
-            />
-            <ColorSwatch
+            isSelected={activeToggleTab === "tab1"}
+            onClick={() => {
+              if (activeToggleTab === "tab1") {
+                setActiveToggleTab("");
+              } else {
+                setActiveToggleTab("tab1");
+              }
+              return false;
+            }}
+          />
+          <ColorSwatch
             square
-
             color="research"
-              isSelected={activeToggleTab === "tab2"}
-              onClick={() => {
-                if (activeToggleTab === "tab2") {
-                  setActiveToggleTab("");
-                } else {
-                  setActiveToggleTab("tab2");
-                }
-                return false;
-              }}
-            />
-            <ColorSwatch
+            isSelected={activeToggleTab === "tab2"}
+            onClick={() => {
+              if (activeToggleTab === "tab2") {
+                setActiveToggleTab("");
+              } else {
+                setActiveToggleTab("tab2");
+              }
+              return false;
+            }}
+          />
+          <ColorSwatch
             square
-
             color="bi"
-              isSelected={activeToggleTab === "tab3"}
-              onClick={() => {
-                if (activeToggleTab === "tab3") {
-                  setActiveToggleTab("");
-                } else {
-                  setActiveToggleTab("tab3");
-                }
-                return false;
-              }}
-            />
-          </Picker>
-        );
-      });
-    })
+            isSelected={activeToggleTab === "tab3"}
+            onClick={() => {
+              if (activeToggleTab === "tab3") {
+                setActiveToggleTab("");
+              } else {
+                setActiveToggleTab("tab3");
+              }
+              return false;
+            }}
+          />
+        </Picker>
+      );
+    });
+  });
