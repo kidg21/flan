ae4ba13077a8966027916b8036b936d25fc9d4c5
Greg Person <gperson@digmap.com>
2020-05-07 18:05:50 -0700

    Icon - Aligned icons vertically in their containers - Linked Icons were overflowing their horizontal bounds

--- src/components/atoms/Icon/Icon.jsx
+++ src/components/atoms/Icon/Icon.jsx
@@ -11,6 +11,7 @@ import Badge from "atoms/Badge";
 
 const LinkedIcon = styled.a`
   color: ${(props) => { return props.theme.text.link; }};
+  width: max-content;
   cursor: ${(props) => {
     if (props.disabled) {
       return "not-allowed";
@@ -41,6 +42,7 @@ const StyledIcon = styled(FontAwesomeIcon)`
   color: ${(props) => {
     return props.theme.palette[props.color] || "";
   }};
+  vertical-align: middle;
   transition: all 0.25s ease-in-out;
 `;
 
@@ -259,8 +261,6 @@ function Icon({
       color = "brand3";
     } if (brand === "brand4") {
       color = "brand4";
-    } else {
-      color = null;
     }
   }
 
