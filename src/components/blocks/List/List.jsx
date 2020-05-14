3906a601e3815dbedd68f896f4a4f40accba31aa
mwalker-dmp <mwalker@digmap.com>
2020-05-14 13:47:55 -0700

    - Add "ContentSection" to Page, (to provide explicit height for content that needs it). - Allow List to specify padding for each ListItem (instead of hard-coding to 0).

--- src/components/blocks/List/List.jsx
+++ src/components/blocks/List/List.jsx
@@ -10,7 +10,7 @@ import Icon from "atoms/Icon";
 import Checkbox from "atoms/Checkbox";
 import Switch from "atoms/Switch";
 import Text, { Title, Label } from "base/Typography";
-import { InteractiveContext, DisabledContext } from "States";
+import { InteractiveContext, DisabledContext, PaddingContext } from "States";
 
 const ListTitle = styled(Label)`
   color: ${(props) => {
@@ -69,13 +69,15 @@ const ListItemWrapper = styled.li`
 `;
 
 function List({
-  children, id, isDivided, isInteractive, title,
+  children, id, isDivided, isInteractive, title, padding,
 }) {
   return (
     <InteractiveContext.Provider value={isInteractive}>
       <ListWrapper isDivided={isDivided} id={id}>
         <ListTitle text={title} />
-        {children}
+        <PaddingContext.Provider value={padding}>
+          {children}
+        </PaddingContext.Provider>
       </ListWrapper>
     </InteractiveContext.Provider>
   );
@@ -86,6 +88,7 @@ List.propTypes = {
   id: PropTypes.string,
   isDivided: PropTypes.bool,
   isInteractive: PropTypes.bool,
+  padding: PropTypes.string,
   title: PropTypes.string,
 };
 List.defaultProps = {
@@ -93,6 +96,7 @@ List.defaultProps = {
   id: null,
   isDivided: false,
   isInteractive: false,
+  padding: "0",
   title: null,
 };
 
@@ -197,7 +201,7 @@ function ListItem({
     >
       <DisabledContext.Provider value={disabled}>
         <Bar
-          padding="0"
+          padding={useContext(PaddingContext)}
           center={{
             content: centerContent,
             align: "left",
