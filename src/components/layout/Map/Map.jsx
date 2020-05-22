a274600126bf2f30a0f7915aa8e33b00237982bf
Greg Person <gperson@digmap.com>
2020-05-21 18:51:19 -0700

    Map - Needs a wrapper to inherit and pass height from a parent layout container

--- src/components/layout/Map/Map.jsx
+++ src/components/layout/Map/Map.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable linebreak-style */
 import React from "react";
 import ReactMapboxGl from "react-mapbox-gl";
+import styled from "styled-components";
+
+const Region = styled.section`
+    flex: auto;
+    width: 100%;
+    height: 100%;
+`;
 
 const Map = ReactMapboxGl({
   accessToken:
@@ -11,14 +18,16 @@ const v1 = [-74.006, 40.7128];
 
 function Mapbox() {
   return (
-    <Map
-      style="mapbox://styles/lizgallagher/cjvwt6yv316cl1cpgoj94jvma?optimize=true"
-      center={v1}
-      containerStyle={{
-        height: "100%",
-        width: "100%",
-      }}
-    />
+    <Region>
+      <Map
+        style="mapbox://styles/lizgallagher/cjvwt6yv316cl1cpgoj94jvma?optimize=true"
+        center={v1}
+        containerStyle={{
+          height: "100%",
+          width: "100%",
+        }}
+      />
+    </Region>
   );
 }
 
