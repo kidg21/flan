98b96b4e7e5f06a63000bb9ac2b20f4a871eb9e6
Greg Person <gperson@digmap.com>
2020-05-22 13:38:13 -0700

    Map - Wrapper was unecessary for the Map to receive dimensions from its parent container

--- src/components/layout/Map/Map.jsx
+++ src/components/layout/Map/Map.jsx
@@ -1,33 +1,30 @@
 /* eslint-disable linebreak-style */
 import React from "react";
 import ReactMapboxGl from "react-mapbox-gl";
-import styled from "styled-components";
 
-const Region = styled.section`
-    flex: auto;
-    width: 100%;
-    height: 100%;
-`;
+const container = ({
+  height: "100%",
+  width: "100%",
+});
 
 const Map = ReactMapboxGl({
   accessToken:
     "pk.eyJ1IjoibGl6Z2FsbGFnaGVyIiwiYSI6ImNqcHZpNm0xNjAxaTMzeG80eWRjcWxscnYifQ.HB1ie-iccCc1TlpKJXUZKw",
 });
 
+const map = ({
+  style: "mapbox://styles/lizgallagher/cjvwt6yv316cl1cpgoj94jvma?optimize=true",
+});
+
 const v1 = [-74.006, 40.7128];
 
 function Mapbox() {
   return (
-    <Region>
-      <Map
-        style="mapbox://styles/lizgallagher/cjvwt6yv316cl1cpgoj94jvma?optimize=true"
-        center={v1}
-        containerStyle={{
-          height: "100%",
-          width: "100%",
-        }}
-      />
-    </Region>
+    <Map
+      containerStyle={container}
+      style={map.style}
+      center={v1}
+    />
   );
 }
 
