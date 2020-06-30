1190fbbfc5c4373e903eca710f0a1614fe44dd59
egallagher <egallagher@digmap.com>
2020-06-30 15:04:54 -0700

    added variant to prop text

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -333,7 +333,7 @@ Button.propTypes = {
   label: PropTypes.string,
   onClick: PropTypes.func,
   type: PropTypes.oneOf(["button", "reset", "submit"]),
-  variant: PropTypes.oneOf(["action", "alert", "info", "success", "warning"]),
+  variant: PropTypes.oneOf(["action", "alert", "info", "success", "warning", "neutral"]),
 };
 
 Button.defaultProps = {
