e5f4b1284560972947b1ae81f8aee8dde1c639ef
egallagher <egallagher@digmap.com>
2020-07-21 19:26:30 -0700

    app story return to normal

--- src/components/templates/Applications/Landvision/Application.stories.js
+++ src/components/templates/Applications/Landvision/Application.stories.js
@@ -633,6 +633,7 @@ storiesOf("Templates|Applications/Appraisal", module)
                               content:(<Text text="Data Source: EASI, Last Updated: 01/01/2020 " size="sm"/>),
                             align: "left",}}
                             />}>
+                              
                               <Form>
                               <Field label="Parcel Link" value={<Link text="808-23093"/>}/>
                               
