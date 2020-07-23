00684e888b6b6faaca91b970b3b4cfeab27d5f87
egallagher <egallagher@digmap.com>
2020-07-23 10:26:39 -0700

    updates

--- src/components/templates/Applications/Landvision/Application.stories.js
+++ src/components/templates/Applications/Landvision/Application.stories.js
@@ -253,7 +253,6 @@ storiesOf("Templates|Applications/Research", module)
                               </Panel>)
                             ,
                           }}
-                          
                         />
                       ),
                     }}
