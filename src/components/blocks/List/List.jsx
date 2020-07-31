f89fb310863b13ef9f3ec02910eb7e317fcaeeee
egallagher <egallagher@digmap.com>
2020-07-30 18:24:47 -0700

    updated on Click

--- src/components/blocks/List/List.jsx
+++ src/components/blocks/List/List.jsx
@@ -158,7 +158,7 @@ function getRightContent(post, disabled, onClick) {
       rightContent = {
         content: post,
         width: "max-content",
-        onClick: sectionOnClick || onClick,
+        onClick: post.OnClick || onClick,
       };
     }
     return rightContent;
@@ -207,7 +207,7 @@ function getLeftContent(pre, disabled, onClick) {
     leftContent = {
       content: pre,
       width: "max-content",
-      onClick: sectionOnClick || onClick,
+      onClick: pre.OnClick || onClick,
     };
   }
   return leftContent;
