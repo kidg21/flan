cf28cb783b02b28b8c64a91e9e92c0f8d339561c
mwalker-dmp <mwalker@digmap.com>
2020-08-25 18:00:02 -0700

    Enhance Field to accept href / target and pass along to Link element if specified.

--- src/components/atoms/Field/Field.jsx
+++ src/components/atoms/Field/Field.jsx
@@ -71,7 +71,18 @@ const Section = styled.section`
 `;
 
 function Field({
-  align, className, disabled, id, label, onChange, onClick, value, labelWidth, valueWidth,
+  align,
+  className,
+  disabled,
+  href,
+  id,
+  label,
+  onChange,
+  onClick,
+  target,
+  value,
+  labelWidth,
+  valueWidth,
 }) {
   let fieldColumns;
   let fieldGap;
@@ -99,7 +110,6 @@ function Field({
       break;
   }
 
-
   let field = (
     <FieldValue
       onChange={onChange}
@@ -110,9 +120,9 @@ function Field({
     </FieldValue>
   );
 
-  if (onClick) {
+  if (href || onClick) {
     field = (
-      <Link disabled={disabled} size="lg" onClick={onClick}>
+      <Link disabled={disabled} size="lg" href={href} target={href ? target : undefined} onClick={onClick}>
         {field}
       </Link>
     );
@@ -137,11 +147,13 @@ Field.propTypes = {
   valueWidth: PropTypes.oneOf(["auto", "1x", "2x", "3x", "4x"]),
   className: PropTypes.string,
   disabled: PropTypes.bool,
+  href: PropTypes.string,
   id: PropTypes.string,
   label: PropTypes.string.isRequired,
   /** Not currently being used but staying put for the next iteration. */
   onChange: PropTypes.func,
   onClick: PropTypes.func,
+  target: PropTypes.string,
   value: PropTypes.node,
 };
 
@@ -151,9 +163,11 @@ Field.defaultProps = {
   valueWidth: "1x",
   className: null,
   disabled: false,
+  href: undefined,
   id: null,
   onChange: null,
   onClick: null,
+  target: "_blank",
   value: null,
 };
 
