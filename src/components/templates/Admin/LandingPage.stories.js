fe3fabe3736aaeff0fc68c71a5adf94d59b7e763
egallagher <egallagher@digmap.com>
2020-09-07 16:23:51 -0700

    tweaks to card grid

--- src/components/templates/Admin/LandingPage.stories.js
+++ src/components/templates/Admin/LandingPage.stories.js
@@ -6,8 +6,10 @@ import MainPanelHeader from "elements/PanelHeaders/MainPanelHeader";
 import Tabs, { TabItem } from "blocks/Tabs";
 import Legend from "blocks/Legend";
 import Layout from "layout/Layout";
+import Flex from "layout/Flex";
 import Bar from "layout/Bar";
-import Text from "base/Typography";
+import Text, {Title} from "base/Typography";
+import Icon from "atoms/Icon";
 import Image from "atoms/Image";
 import Card, { CardGrid } from "elements/Card";
 import Grid from "layout/Grid";
@@ -76,20 +78,59 @@ storiesOf("Templates|Landing Page", module)
                     template="B_03"
                     A={{
                       id: "A",
-                      content: (<Image src={Chicago} alt="Chicao" width="fit-content" />),
+                      content: (<React.Fragment>
+                        <Template
+                          isOverlay
+                          id="Template A_01"
+                          template="A_01"
+                          A={{
+                            id: "A",
+                            content: (<Flex justifyContent="center" flexDirection="columns" alignItems="center" ><Title size="4xl" text="Welcome to Your App Suite"/></Flex>),
+                          }}
+                        />
+                        <Image src={Chicago} alt="Chicago" />
+                      </React.Fragment>),
                     }}
                     B={{
                       id: "B",
                       content: (
-                      <Panel>
-                          <Bar padding="2x" center={
-                        <CardGrid columns="4">
-                          <Card body={<Text text="JOBS" />} />
-                          <Card body={<Text text="RESEARCH" />} />
-                          <Card body={<Text text="REPORT WRITER" />} />
-                          <Card body={<Text text="INSIGHTS" />} />
-                        </CardGrid>}/>
-                      </Panel>),
+                        <Panel>
+                          <Bar
+                            padding="2x"
+                            center={(
+                              <CardGrid columns="4">
+                                <Card body={(
+                                  <Grid columns="1">
+                                    <Bar center={<Icon icon="star" size="xl" />} />
+                                    <Bar center={<Text text="JOBS" />} />
+                                  </Grid>
+                                  )}
+                                />
+                                <Card body={(
+                                  <Grid columns="1">
+                                    <Bar center={<Icon icon="star" size="xl" />} />
+                                    <Bar center={<Text text="RESEARCH" />} />
+                                  </Grid>
+                                  )}
+                                />
+                                <Card body={(
+                                  <Grid columns="1">
+                                    <Bar center={<Icon icon="star" size="xl" />} />
+                                    <Bar center={<Text text="REPORT WRITER" />} />
+                                  </Grid>
+                                  )}
+                                />
+                                <Card body={(
+                                  <Grid columns="1">
+                                    <Bar center={<Icon icon="star" size="xl" />} />
+                                    <Bar center={<Text text="INSIGHTS" />} />
+                                  </Grid>
+                                  )}
+                                />
+                              </CardGrid>
+                      )}
+                          />
+                        </Panel>),
 
                     }}
                   />
